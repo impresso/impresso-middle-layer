e304b4b11b63b210309ef55931a011e9726fd062
Daniele Guido <gui.daniele@gmail.com>
2018-01-10 10:52:22 +0100

    fix #1
    cfr. option at
    https://github.com/neo4j/neo4j-javascript-driver/blob/1.6/src/v1/index.j
    s#L122

--- src/neo4j.js
+++ src/neo4j.js
@@ -8,7 +8,9 @@ module.exports = function () {
   const app = this;
 
   const config = app.get('neo4j'),
-        driver = neo4j.driver(config.host, neo4j.auth.basic(config.auth.user, config.auth.pass));
+        driver = neo4j.driver(config.host, neo4j.auth.basic(config.auth.user, config.auth.pass),{
+          connectionPoolSize: 0
+        });
         
   
   const runner = (cypherQuery, params) => {
