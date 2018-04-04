7bd26b09dfd5e1c9b42b4ee3ce5893bfb4351574
Daniele Guido <gui.daniele@gmail.com>
2018-04-04 12:07:49 +0200

    point to the correct neo4jToInt function

--- src/hooks/neo4j.js
+++ src/hooks/neo4j.js
@@ -1,3 +1,5 @@
+const { neo4jToInt } = require('../services/neo4j.utils');
+
 const normalizeTimeline = () => {
   return async context => {
     context.result = context.result.records.map(record => {
