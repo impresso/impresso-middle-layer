21d79e2b9833676467eb449b93733c094392165b
Daniele Guido <gui.daniele@gmail.com>
2018-04-28 15:40:30 +0200

    add neo4j hooks

--- src/hooks/neo4j.js
+++ src/hooks/neo4j.js
@@ -1,4 +1,6 @@
 const { neo4jToInt } = require('../services/neo4j.utils');
+const { BadRequest } = require('@feathersjs/errors');
+const debug = require('debug')('impresso/hooks:neo4j');
 
 const normalizeTimeline = () => {
   return async context => {
@@ -11,20 +13,47 @@ const normalizeTimeline = () => {
   }
 }
 
+const parseJsonProperty = (name) => {
+  return async context => {
+    // context.result = context.result.records.map(record => {
+    //   return {
+    //     t: neo4jToInt(record._fields[0]),
+    //     w: neo4jToInt(record._fields[1]),
+    //   }
+    // })
+    debug(`parseJsonProperty: <${name}> parsed correctly.`)
+  }
+}
+
+const raiseErrorIfEmpty = (explanation={}) => {
+  return async context => {
+    if(Array.isArray(context.result) && !context.result.length) {
+      debug('raiseErrorIfEmpty: apparently context.result is empty!', context.result)
+      throw new BadRequest('empty context.result', explanation);
+    } else {
+      debug('raiseErrorIfEmpty: context.result ok, proceed.')
+    }
+  }
+}
+
 const normalizeEmptyRecords = () => {
   return async context => {
     // only when empty array are given
-    console.log('ooooo', context.result)
     if(Array.isArray(context.result) && !context.result.length) {
+      debug('normalizeEmptyRecords: apparently context.result is empty!', context.result)
       context.result = {
         count: 0,
         records : []
       }
+    } else {
+      debug('normalizeEmptyRecords: context.result ok, proceed.')
     }
   }
 }
 
 module.exports = {
   normalizeTimeline,
-  normalizeEmptyRecords
+  normalizeEmptyRecords,
+  raiseErrorIfEmpty,
+  parseJsonProperty
 }
