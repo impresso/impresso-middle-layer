be516e24a3dfe64e9c8cfd97c3cf6b48b7cc88c6
Daniele Guido <gui.daniele@gmail.com>
2018-05-21 15:49:25 +0200

    add iiid proxy to enrich data with proxied IIIF urls

---
+++ src/hooks/iiif.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const debug = require('debug')('impresso/hooks:iiif');
+
+const _getIIIF = (context, page__uid) => {
+  const proxyhost = context.app.get('proxy').host;
+  return `${proxyhost}/proxy/iiif/${page__uid}/info.json`
+}
+
+// use this hook to add IIIF endpoints that go well with IIIF proxy.
+const proxyIIIF = () => {
+  return async context => {
+    if (context.type !== 'after') {
+      throw new Error(`The 'proxyIIIF' hook should only be used as a 'after' hook.`);
+    }
+
+    if (context.result && context.result.uid) {
+      debug(`proxyIIIF: <uid>: ${context.result.uid}`);
+      context.result.iiif = _getIIIF(context, context.result.uid)
+    } else if (context.result.data) {
+      debug(`proxyIIIF: with result.data <length>: ${context.result.data.length}`);
+      for (let page of context.result.data) {
+        page.iiif = _getIIIF(context, page.uid)
+      }
+    } else {
+      debug('proxyIIIF: unable to find an UID to generate the IIIF')
+    }
+    // if(context.result) {
+    //
+    //   console.log(context.result);
+    //   //
+    //   // for(let i in context.result.pages) {
+    //   //   context.result.pages[i].iiif =
+    //   // }
+    // }
+  }
+}
+
+
+module.exports = {
+  proxyIIIF,
+}
