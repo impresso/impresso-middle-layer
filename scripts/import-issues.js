7f7499453f74d4376e0275f2e41a2aa90d5a6628
Daniele Guido <gui.daniele@gmail.com>
2018-05-21 15:59:59 +0200

    add custom waterfall for issues and pages services

--- scripts/import-issues.js
+++ scripts/import-issues.js
@@ -1,17 +1,26 @@
-const {merge} = require('./bulk');
+const {merge, count, apoc} = require('./bulk');
 const debug = require('debug')('impresso/scripts:import-issues');
 
 debug('start!');
-merge('issues', (item) => {
-  // get day date out of uids. This way we validate uids
-  const date = item.uid.match(/[A-Z]{3,}\-(\d{4}\-\d{2}\-\d{2})/)
-  return {
-    uid: item.uid,
-    year: item.year,
-    date: date[1],
-    newspaper_uid: item.newspaper_uid
-  }
-}, 500).then(res => {
+async function waterfall() {
+  debug('merging issues...');
+  const merging = await merge('issues', (item) => {
+    const date = item.uid.match(/[A-Z]{3,}\-(\d{4}\-\d{2}\-\d{2})/)
+    return {
+      uid: item.uid,
+      year: item.year,
+      date: date[1],
+      newspaper_uid: item.newspaper_uid
+    }
+  }, 500);
+  debug('merge issues done.');
+  debug('setting count ...');
+
+  await count('issues');
+  await apoc('issues', 'APOC_set_newspaper__count_issues');
+}
+
+waterfall().then(res => {
   debug('done, exit.');  // prints 60 after 2 seconds.
   process.exit();
 }).catch(err => {
