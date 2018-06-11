eed9d3ea9b9291a685274c01c7fb4032940ced2c
Daniele Guido <gui.daniele@gmail.com>
2018-06-11 10:12:42 +0200

    lint

--- src/hooks/gravatar.js
+++ src/hooks/gravatar.js
@@ -9,8 +9,8 @@ const gravatarUrl = 'https://s.gravatar.com/avatar';
 // The size query. Our chat needs 60px images
 const query = 's=60';
 
-module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-  return function gravatar (hook) {
+module.exports = function(options = {}) { // eslint-disable-line no-unused-vars
+  return function gravatar(hook) {
     const { email } = hook.data;
     // Gravatar uses MD5 hashes from an email address to get the image
     const hash = crypto.createHash('md5').update(email).digest('hex');
