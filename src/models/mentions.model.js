740fa1d849fa288c619cfb338a7b60bfce2667e7
Daniele Guido <gui.daniele@gmail.com>
2018-11-21 08:13:40 +0100

    eslint mentions

--- src/models/mentions.model.js
+++ src/models/mentions.model.js
@@ -1,21 +1,21 @@
 const MENTION_TYPES = [
   'Person',
-  'Location'
-]
+  'Location',
+];
 
 class Mention {
   constructor({
     name = '',
     frequence = 0,
-    type = ''
+    type = '',
   } = {}) {
     this.name = String(name);
     this.frequence = parseInt(frequence, 10);
-    this.type = MENTION_TYPES.indexOf(type) !== -1? type.toLowerCase(): 'mention';
+    this.type = MENTION_TYPES.indexOf(type) !== -1 ? type.toLowerCase() : 'mention';
   }
 }
 
-const solrFactory = res => (doc) => new Mention({
+const solrFactory = () => doc => new Mention({
   name: doc.l_s,
   type: doc.t_s,
   frequence: doc.fq_i,
