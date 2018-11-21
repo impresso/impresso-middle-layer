ce6280e989affe2d983b23013a2bacf81fbe540d
Daniele Guido <gui.daniele@gmail.com>
2018-11-21 07:27:33 +0100

    Update src/services/pages/pages.class.js
    to match the model. WIP to include tags and collections if an user is authentified.

--- src/services/pages/pages.class.js
+++ src/services/pages/pages.class.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
+const debug = require('debug')('impresso/services:pages');
+const { NotFound } = require('@feathersjs/errors');
 const Neo4jService = require('../neo4j.service').Service;
 const solr = require('../../solr');
-const { NotFound } = require('@feathersjs/errors');
+const Article = require('../../models/articles.model');
+const Page = require('../../models/pages.model');
 
 class Service extends Neo4jService {
   constructor(options) {
@@ -11,26 +14,35 @@ class Service extends Neo4jService {
 
   async get(id, params) {
     const results = await Promise.all([
-      // we perform a solr request to get
-      // the full text, regions of the specified article
+      // we perform a solr request to get basic info on the page:
+      // number of articles,
       this.solr.findAll({
-        q: `page_id_ss:${id}}`,
-        fl: article.ARTICLE_SOLR_FL_LITE,
-      }, article.solrFactory),
+        q: `page_id_ss:${id}`,
+        fl: Article.ARTICLE_SOLR_FL_MINIMAL,
+      }),
       // at the same time, we use the neo4jService get to get article instance from our graph db
+      // user tags, user collections and public collections
       super.get(id, {
         ...params,
-        findAll: uids.length > 1,
+        findAll: true,
       }),
       // get from db
     ]);
 
-    if (results[0].response.numFound !== 1) {
+    if (results[0].response.numFound === 0) {
       throw new NotFound();
     }
+    // initialize page after solr
+    const pag = new Page.Model({
+      uid: id,
+      countArticles: results[0].response.numFound,
+    });
 
+
+    return {
+      ...pag,
+    };
   }
-
 }
 
 module.exports = function (options) {
