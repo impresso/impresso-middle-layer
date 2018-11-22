6a68d3154b8b5592029bfff9d28192915de8383d
Daniele Guido <gui.daniele@gmail.com>
2018-11-22 18:00:30 +0100

    find and get method use sequelize

--- src/services/users/users.class.js
+++ src/services/users/users.class.js
@@ -2,11 +2,40 @@
 const debug = require('debug')('impresso/services:users');
 const { neo4jRecordMapper } = require('../neo4j.utils.js');
 const Neo4jService = require('../neo4j.service').Service;
-const errors = require('@feathersjs/errors');
+const { BadRequest, NotFound } = require('@feathersjs/errors');
 const { encrypt } = require('../../crypto');
 const shorthash = require('short-hash');
+const sequelize = require('../../sequelize');
+const User = require('../../models/users.model');
+
 
 class Service extends Neo4jService {
+  constructor(options) {
+    super(options);
+    const sequelizeConfig = options.app.get('sequelize');
+    // sequelize
+    this.sequelize = sequelize.client(sequelizeConfig);
+    this.sequelizeKlass = User.sequelize(this.sequelize, {
+      tableName: sequelizeConfig.tables.users,
+    });
+    console.log(this.sequelizeKlass);
+  }
+
+  async get(id, params) {
+    // if you're staff; otherwise get your own.
+    const user = await this.sequelizeKlass.scope('isActive', 'get').findOne({
+      where: {
+        '$profile.uid$': id,
+      },
+    });
+    if (!user) {
+      debug(`get '${this.name}': uid not found <uid>:`, id);
+      throw new NotFound();
+    } else {
+      return new User(user.toJSON());
+    }
+  }
+
   async create(data, params) {
     // create multiple only if user is admin @todo!
     // if (Array.isArray(data)) {
@@ -30,7 +59,7 @@ class Service extends Neo4jService {
       user.username = data.sanitized.username;
       Object.assign(user, encrypt(data.sanitized.password));
     } else {
-      throw new errors.BadRequest({
+      throw new BadRequest({
         message: 'MissingParameters',
       });
     }
@@ -81,25 +110,52 @@ class Service extends Neo4jService {
 
   async find(params) {
     const userUid = params.authenticated ? params.user.uid : undefined;
-
+    debug('find: ', params);
     let uid;
 
     if (params.sanitized.githubId) {
       uid = `github-${params.sanitized.githubId}`;
     } else if (params.sanitized.email) {
       uid = params.sanitized.email;
+    } else if (params.sanitized.uid) {
+      uid = params.sanitized.uid;
     }
-    const result = await this._run(this.queries.find, {
-      ...params.query,
-      uid,
-      user_uid: userUid,
-    }).then(res =>
-      // add id field for oauth2. @todo change somewhere in config
-      res.records.map(neo4jRecordMapper).map(u => ({
-        ...u,
-        id: u.uid,
-      })));
-    return result;
+
+    let sequelizeParams = {};
+
+    // e.g. during authentication process
+    if(uid) {
+      const sequelizeParams = {
+        where: {
+          $or: [{ email: uid,},{username: uid,},{'$profile.uid$': uid,},],
+        },
+      }
+    }
+
+    return this.sequelizeKlass.scope('isActive', 'find')
+      .findAll(sequelizeParams)
+      .then(res => res.map(d => new User(d.toJSON())));
+    // const results = await Promise.all([
+    //   // look for username or email or profile uid (sooo cool!)
+    //
+    //   // neo4j!
+    //   this._run(this.queries.find, {
+    //     ...params.query,
+    //     uid,
+    //     user_uid: userUid,
+    //   }).then(res =>
+    //     // add id field for oauth2. @todo change somewhere in config
+    //     res.records.map(neo4jRecordMapper).map(u => ({
+    //       ...u,
+    //       id: u.uid,
+    //   }))),
+    // ]);
+    //
+    // if(results[1]) {
+    //
+    // }
+    // console.log(results);
+    // return results[0];
   }
 }
 
