be65d6be95c849003d25e4cfe15fa32b34a854d0
Daniele Guido <gui.daniele@gmail.com>
2018-12-03 11:19:25 +0100

    fix user generation

--- test/services/utils.js
+++ test/services/utils.js
@@ -1,6 +1,27 @@
 const app = require('../../src/app');
+const sequelize = require('../../src/sequelize');
+
+const Collection = require('../../src/models/collections.model');
+const User = require('../../src/models/users.model');
 
 const removeGeneratedUser = async (user) => {
+  const client = sequelize.client(app.get('sequelize'));
+  // get user if any
+  const userInDb = await User.sequelize(client).findOne({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (userInDb) {
+    await Collection.sequelize(client).destroy({
+      where: {
+        creator_id: userInDb.id,
+      },
+    });
+  }
+
+  // remove all
   await app.service('users').remove(user.username, {
     user: {
       is_staff: true,
@@ -10,7 +31,9 @@ const removeGeneratedUser = async (user) => {
 
 const generateUser = async (user) => {
   await removeGeneratedUser(user);
-  const result = await app.service('users').create(user);
+  const result = await app.service('users').create({
+    ...user,
+  });
   return result;
 };
 
