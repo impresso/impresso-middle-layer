39f7b04d8146ace37ea6c3e63782bf8b96f484cd
Daniele Guido <gui.daniele@gmail.com>
2018-12-19 22:16:10 +0100

    refactor article class to take into account new mixins

--- src/services/articles/articles.class.js
+++ src/services/articles/articles.class.js
@@ -1,27 +1,42 @@
 const Neo4jService = require('../neo4j.service').Service;
+const SequelizeService = require('../sequelize.service');
+const SolrService = require('../solr.service');
+
 const solr = require('../../solr');
 const { NotFound } = require('@feathersjs/errors');
-const article = require('../../models/articles.model');
+const Article = require('../../models/articles.model');
 const Tag = require('../../models/tags.model').Model;
 
-class Service extends Neo4jService {
-  constructor(options) {
-    super(options);
-    this.solr = solr.client(options.app.get('solr'));
+class Service {
+  constructor({
+    name = '',
+    app,
+  } = {}) {
+    this.name = String(name);
+    this.app = app;
+    this.SequelizeService = SequelizeService({
+      app,
+      name,
+    });
+    this.SolrService = SolrService({
+      app,
+      name,
+      namespace: 'search',
+    });
   }
 
   async find(params) {
-    let fl = article.ARTICLE_SOLR_FL_LITE;
+    let fl = Article.ARTICLE_SOLR_FL_LITE;
     let pageUids = [];
 
     if (params.isSafe) {
       pageUids = params.query.filters
         .filter(d => d.type === 'page')
         .map(d => d.q);
-      // As we requested a page,
+      // As we requested article in a page,
       // we have to calculate regions for that page.
       if (pageUids.length === 1) {
-        fl = article.ARTICLE_SOLR_FL;
+        fl = Article.ARTICLE_SOLR_FL;
       }
     }
     // if(params.isSafe query.filters)
@@ -31,7 +46,7 @@ class Service extends Neo4jService {
       skip: params.query.skip,
       fl,
       order_by: 'id asc', // default ordering TODO
-    }, article.solrFactory);
+    }, Article.solrFactory);
 
     if (results.response.numFound === 0) {
       throw new NotFound();
@@ -62,36 +77,65 @@ class Service extends Neo4jService {
   }
 
   async get(id, params) {
-    const uids = id.split(',');
+    const where = {
+      uid: id,
+    };
 
-    const results = await Promise.all([
+    // if there's an user, get the private ones as well.
+    if(params.user) {
+      where['$collections.creator_id$'] = params.user.id;
+    } else {
+      where['$collections.status$'] = {
+        $in: ['PUB', 'SHA'],
+      }
+    }
+
+    const article = await Promise.all([
       // we perform a solr request to get
       // the full text, regions of the specified article
-      this.solr.findAll({
-        q: `id:${uids.join(' OR ')}`,
-        fl: uids.length > 1 ? article.ARTICLE_SOLR_FL_LITE : article.ARTICLE_SOLR_FL,
-      }, article.solrFactory),
-      // at the same time, we use the neo4jService get to get article instance from our graph db
-      super.get(id, {
-        ...params,
-        findAll: uids.length > 1,
+      this.SolrService.get(id, {
+        fl: Article.ARTICLE_SOLR_FL
       }),
-    ]);
 
-    if (results[0].response.numFound !== 1) {
-      throw new NotFound();
-    }
+      // get the newspaper, then the collections
+      this.SequelizeService.get(id, {
+        scope: 'get',
+        where,
+      }),
+      // at the same time, we use the neo4jService get to get article instance from our graph db
+      // super.get(id, {
+      //   ...params,
+      //   findAll,
+      // }),
+      //
+
+    ]).then(results => {
+      return {
+        ... results[0],
+        v: results[1].v,
+        newspaper: results[1].newspaper,
+        collections: results[1].collections.filter(d => d.status === 'PUB'),
+      }
+    });
+
+    return article;
+    // if (results[0].response.numFound !== 1) {
+    //   throw new NotFound();
+    // }
+
+    // get all related collections
+    // this.app
 
     // expect an array indeed
-    if (uids.length > 1) {
-      return results[0].response.docs;
-    }
-    // enrich with neo4j results
-    return {
-      ...results[0].response.docs[0],
-      // add tags
-      tags: results[1] ? results[1].tags.map(d => new Tag(d)) : [],
-    };
+    // if (uids.length > 1) {
+    // return results[0].response.docs;
+    // }
+    // // enrich with neo4j results
+    // return {
+    //   ...results[0].response.docs[0],
+    //   // add tags
+    //   tags: results[1] ? results[1].tags.map(d => new Tag(d)) : [],
+    // };
 
     // // if params findall was true or when multiple ids are given, results[1] is an array.
     // if (Array.isArray(results[1].data) && results[1].data.length) {
