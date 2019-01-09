eec3900e7c5354f8bb56c64142d5f53429e4f0f1
Daniele Guido <gui.daniele@gmail.com>
2019-01-09 09:27:25 +0100

    refactor issue service with custom issues class

---
+++ src/services/issues/issues.class.js
@@ -0,0 +1,62 @@
+const debug = require('debug')('impresso/services:newspapers');
+const SequelizeService = require('../sequelize.service');
+const SolrService = require('../solr.service');
+const Issue = require('../../models/issues.model');
+
+class Service {
+  constructor({
+    app,
+    name = '',
+  } = {}) {
+    this.name = String(name);
+    this.app = app;
+    this.SequelizeService = SequelizeService({
+      app,
+      name,
+    });
+    this.SolrService = SolrService({
+      app,
+      name,
+      namespace: 'search',
+    });
+  }
+
+  async find(params) {
+    debug(`find '${this.name}': with params.isSafe:${params.isSafe} and params.query:`, params.query);
+    const results = await this.SolrService.find({
+      ...params,
+      fl: Issue.ISSUE_SOLR_FL_MINIMAL,
+      collapse_by: 'meta_issue_id_s',
+      // get first ARTICLE result
+      collapse_fn: 'sort=\'id ASC\'',
+    });
+    // add SequelizeService to load Newspaper properly.
+    return results;
+  }
+
+  async get(id) {
+    const issue = await Promise.all([
+      // we perform a solr request to get
+      // the full text, regions of the specified article
+      this.SolrService.find({
+        query: {
+          limit: 1,
+          skip: 0,
+        },
+        q: `meta_issue_id_s:${id}`,
+        fl: Issue.ISSUE_SOLR_FL_MINIMAL,
+        ollapse_by: 'meta_issue_id_s',
+        // get first ARTICLE result
+        collapse_fn: 'sort=\'id ASC\'',
+      }).then(res => res.data[0]),
+    ]).then(results =>
+      // TODO
+      results[0]);
+    return issue;
+  }
+}
+
+module.exports = function (options) {
+  return new Service(options);
+};
+module.exports.Service = Service;
