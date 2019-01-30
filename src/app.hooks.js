24fd90f1105537d6babab112b25dc36d45e2a0fc
Daniele Guido <gui.daniele@gmail.com>
2019-01-30 08:55:06 +0100

    add basic query params if no query is specified

--- src/app.hooks.js
+++ src/app.hooks.js
@@ -3,14 +3,24 @@ const logger = require('./hooks/logger');
 const { validateRouteId } = require('./hooks/params');
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const basicParams = () => (context) => {
+  if (!context.params.query) {
+    context.params.query = {};
+  }
+};
+
 const hooks = {
   before: {
     all: [
       validateRouteId(),
     ],
-    find: [],
+    find: [
+
+    ],
     get: [],
-    create: [],
+    create: [
+      basicParams(),
+    ],
     update: [],
     patch: [],
     remove: [],
