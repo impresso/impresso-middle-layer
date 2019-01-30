896837ef97001d12d19196202a1e64b94a9e71eb
Daniele Guido <gui.daniele@gmail.com>
2019-01-30 08:51:13 +0100

    Add doc

--- src/hooks/results.js
+++ src/hooks/results.js
@@ -1,35 +1,35 @@
 const debug = require('debug')('impresso/hooks:results');
 
 /**
- * [resolve description]
- * @param  {[type]} service optional; if defined, context.result.toBeResolved will be filtered for tis serivce
+ * If a service is given, context.result.toBeResolved
+ * will be filtered for serivce
+ * @param  {[type]} service optional;
  * @return {[type]}         [description]
  */
-const resolve = (service) => async(context) => {
-  if(context.result.toBeResolved && context.result.toBeResolved.length) {
-    if(service) {
+const resolve = service => async (context) => {
+  if (context.result.toBeResolved && context.result.toBeResolved.length) {
+    if (service) {
       context.result.toBeResolved = context.result.toBeResolved.filter(d => d.service === service);
     }
     debug(`resolve: ${context.result.toBeResolved.length} services to resolve`, context.result.toBeResolved);
-    
+
     context.result.resolved = await Promise.all(context.result.toBeResolved.map(d =>
       context.app.service(d.service).get(d.uids.join(','), {
-       user: context.params.user,
-       findAll: true,
-       query: {
-         limit: d.uids.length,
-       }
-     }).then(results => ({
-       service: d.service,
-       data: results,
-     }))
-   ));
+        user: context.params.user,
+        findAll: true,
+        query: {
+          limit: d.uids.length,
+        },
+      }).then(results => ({
+        service: d.service,
+        data: results,
+      }))));
     // delete context.result.toBeResolved;
   } else {
     debug('resolve: result.toBeResolved, nothing to resolve.');
   }
-}
+};
 
 module.exports = {
   resolve,
-}
+};
