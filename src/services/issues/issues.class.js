da8e095c90492c5d59e099c4c08d3e8fdf11a029
Daniele Guido <gui.daniele@gmail.com>
2019-02-25 10:34:30 +0100

    load issue from mysql

--- src/services/issues/issues.class.js
+++ src/services/issues/issues.class.js
@@ -1,9 +1,8 @@
 const debug = require('debug')('impresso/services:newspapers');
 const SequelizeService = require('../sequelize.service');
 const SolrService = require('../solr.service');
-const Neo4jService = require('../neo4j.service');
-
 const Issue = require('../../models/issues.model');
+const Page = require('../../models/pages.model');
 
 class Service {
   constructor({
@@ -13,10 +12,6 @@ class Service {
     this.name = String(name);
     this.app = app;
     this.SequelizeService = SequelizeService({
-      app,
-      name,
-    });
-    this.Neo4jService = Neo4jService({
       app,
       name,
     });
@@ -39,7 +34,7 @@ class Service {
     // add SequelizeService to load Newspaper properly.
     return results;
   }
-
+  // eslint-disable-next-line no-unused-vars
   async get(id, params) {
     const issue = await Promise.all([
       // we perform a solr request to get
@@ -55,10 +50,36 @@ class Service {
         // get first ARTICLE result
         collapse_fn: 'sort=\'id ASC\'',
       }).then(res => res.data[0]),
-      this.Neo4jService.get(id, params),
-    ]).then(results => ({
+
+      this.SequelizeService.get(id, {
+        scope: 'get',
+      }),
+
+      this.SequelizeService.rawSelect({
+        query: `
+          SELECT pages.id as uid, pages.page_number as num, pages.has_converted_coordinates as hasCC, COUNT(ci.id) as countArticles
+          FROM pages
+            JOIN issues
+              ON pages.issue_id = issues.id
+            JOIN page_contentItem as pci
+              ON pci.page_id = pages.id
+            JOIN content_items as ci
+              ON pci.content_item_id = ci.id
+          WHERE issues.id = :id
+          GROUP BY pages.id
+          ORDER BY num ASC
+
+        `,
+        replacements: {
+          id,
+        },
+      }),
+
+      // this.Neo4jService.get(id, params),
+    ]).then(results => new Issue({
       ...results[0],
       ...results[1] || {},
+      pages: results[2].map(d => new Page(d)),
     }));
     return issue;
   }
