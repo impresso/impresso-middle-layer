9a019b4cf3fd54d04a4fb9d9993c289617da9185
Daniele Guido <gui.daniele@gmail.com>
2019-03-20 09:03:33 +0100

    add images find endpoint

--- src/models/images.model.js
+++ src/models/images.model.js
@@ -1,8 +1,34 @@
+const Page = require('./pages.model');
+const Newspaper = require('./newspapers.model');
+
 class Image {
   constructor({
     uid = '',
+    coords = [],
+    newspaper = new Newspaper(),
+    year = 0,
+    date = new Date(),
+    pages = [],
   } = {}) {
     this.uid = String(uid);
+    this.year = parseInt(year, 10);
+    this.coords = coords;
+    this.pages = pages;
+    this.regions = pages.map((page) => ({
+      pageUid: page.uid,
+      coords: coords,
+    }));
+
+    if (date instanceof Date) {
+      this.date = date;
+    } else {
+      this.date = new Date(date);
+    }
+    if (newspaper instanceof Newspaper) {
+      this.newspaper = newspaper;
+    } else {
+      this.newspaper = new Newspaper(newspaper);
+    }
   }
 
   /**
@@ -12,11 +38,17 @@ class Image {
    */
   static solrFactory() {
     return (doc) => {
+      console.log(doc);
       const img = new Image({
         uid: doc.id,
-        // newspaper: new Newspaper({
-        //   uid: doc.meta_journal_s,
-        // }),
+        newspaper: new Newspaper({
+          uid: doc.newspaper[0],
+        }),
+        year: doc.year[0],
+        coords: doc.iiif_box,
+        pages: doc.iiif_base_url.map((d) => new Page({
+          uid: d.split('/').pop(),
+        })),
       });
       return img;
     };
