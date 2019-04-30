45ee77afbcb440612c1062c950c11a84ed46e2b1
Daniele Guido <gui.daniele@gmail.com>
2019-04-30 13:25:43 +0200

    improve static data generation

--- scripts/update-topics.js
+++ scripts/update-topics.js
@@ -12,15 +12,6 @@ const Topic = require(`../src/models/topics.model`);
 debug('start!');
 
 async function waterfall() {
-  debug('find newspapers...');
-
-  const newspapers = await Newspaper.sequelize(sequelizeClient).scope('all').findAll()
-    .then(results => results.map(d => d.toJSON()))
-    .then(results => lodash.keyBy(results, 'acronym'));
-  debug('saving', Object.keys(newspapers).length, 'newspapers...');
-
-  fs.writeFileSync('./src/data/newspapers.json', JSON.stringify(newspapers));
-  debug('success, saved ./src/data/newspapers.js');
 
   debug('find topics...');
   const topics = await solrClient.findAll({
@@ -32,15 +23,48 @@ async function waterfall() {
   }, Topic.solrFactory)
     .then((result) => {
       debug(`${result.response.numFound} topics found in ${result.responseHeader.QTime} ms`);
-      return result.response.docs;
+      return result.response.docs.map(d => ({
+        ...d,
+        words: lodash.take(d.words, 20),
+      }));
     })
     .then(results => lodash.keyBy(results, 'uid'));
+
+
+
+  debug('get topics links...');
+
+  await Object.keys(topics).reduce(async (promise, topicUid) => {
+  // await ['tmrero-fr-alpha_tp47_fr'].reduce(async (promise, topicUid) => {
+    await promise;
+    debug('topic:', topicUid);
+    const result = await solrClient.findAll({
+      q: `topics_dpfs:${topicUid}`,
+      limit: 0,
+      skip: 0,
+      fl: '*',
+      facets: JSON.stringify({
+        topic: {
+          type: 'terms',
+          field: 'topics_dpfs',
+          mincount: 1,
+          limit: 10,
+          offset: 0,
+          numBuckets: true,
+        },
+      }),
+      namespace: 'search',
+    });
+    topics[topicUid].countItems = result.response.numFound;
+    topics[topicUid].relatedTopics = result.facets.topic.buckets.map(d => ({ uid: d.val, w: d.count }));
+    // enrich topic
+    // console.log(result.facets.topic.buckets);
+  }, Promise.resolve());
 
   debug('saving', Object.keys(topics).length, 'topics...');
 
   fs.writeFileSync('./src/data/topics.json', JSON.stringify(topics));
   debug('success, saved ./src/data/topics.js');
-
 }
 
 waterfall().then((res) => {
