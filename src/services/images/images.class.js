f6b05b83b2af085905b85e1596808b1ad0d4be13
Daniele Guido <gui.daniele@gmail.com>
2019-06-02 22:26:23 +0200

    add param similarTo and vectorType

--- src/services/images/images.class.js
+++ src/services/images/images.class.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+const { NotFound } = require('@feathersjs/errors');
 const debug = require('debug')('impresso/services:images');
 const SolrService = require('../solr.service');
 const Image = require('../../models/images.model');
@@ -19,6 +20,36 @@ class Service {
 
   async find(params) {
     debug(`find '${this.name}': with params.isSafe:${params.isSafe} and params.query:`, params.query);
+    if (params.query.similarTo) {
+      debug('get similarTo vector ...', params.query.similarTo);
+      const signature = await this.SolrService.solr.findAll({
+        q: `id:${params.query.similarTo}`,
+        fl: ['id', `signature:_vector_${params.query.vectorType}_bv`],
+        namespace: 'images',
+        limit: 1,
+      })
+        .then(res => res.response.docs[0].signature)
+        .catch((err) => {
+          throw new NotFound();
+        });
+
+      if (!signature) {
+        throw new NotFound('signature not found');
+      }
+      return this.SolrService.solr.findAll({
+        fq: params.query.sq,
+        form: {
+          q: `{!vectorscoring f="_vector_${params.query.vectorType}_bv" vector_b64="${signature}"}`,
+        },
+        fl: '*,score',
+        namespace: 'images',
+        limit: params.query.limit,
+        skip: params.query.skip,
+        facets: params.query.facets,
+        order_by: 'score DESC',
+      }, Image.solrFactory).then(res => this.SolrService.solr.utils.wrapAll(res));
+    }
+
     return this.SolrService.find({
       ...params,
       fl: Image.SOLR_FL,
