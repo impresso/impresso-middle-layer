e47ee55a3e6272220005d35ffea424b50e321157
Daniele Guido <gui.daniele@gmail.com>
2019-06-17 17:10:29 +0200

    add multer configuration in app

--- src/multer.js
+++ src/multer.js
@@ -1,5 +1,12 @@
-var multer = require('multer');
+const debug = require('debug')('impresso/multer');
+const multer = require('multer');
 
-module.exports = function (options) {
-  return multer(options);
+const getMulterClient = (config) => {
+  const client = multer(config);
+  return client;
+}
+module.exports = function (app) {
+  const config = app.get('multer');
+  app.set('multerClient', getMulterClient(config));
+  debug('Multer is ready!');
 };
