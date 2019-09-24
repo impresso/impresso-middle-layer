f6044ac56b7c2d28506f0b1b2ed484def4551b7c
Daniele Guido <gui.daniele@gmail.com>
2019-09-24 11:09:47 +0200

    add current user service

---
+++ src/services/me/me.hooks.js
@@ -0,0 +1,34 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const { discard } = require('feathers-hooks-common');
+
+module.exports = {
+  before: {
+    all: [authenticate('jwt')],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+
+  after: {
+    all: [discard('password')],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+};
