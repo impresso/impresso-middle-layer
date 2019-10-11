875955aebce9df8bc60f3fedb3a33a39ed0579d6
Daniele Guido <gui.daniele@gmail.com>
2019-10-11 16:06:55 +0200

    generate uid from data md5 hash, add toJSON() sequelize method

--- src/models/search-queries.model.js
+++ src/models/search-queries.model.js
@@ -1,11 +1,32 @@
+const crypto = require('crypto');
 const { DataTypes } = require('sequelize');
 const User = require('./users.model');
 
 class SearchQuery {
   constructor({
     uid = '',
-  } = {}) {
+    name = '',
+    description = '',
+    data = '',
+    creationDate,
+    lastModifiedDate,
+    creator,
+    countItems = -1,
+  }) {
     this.uid = String(uid);
+    this.name = String(name);
+    this.description = String(description);
+    this.data = String(data);
+    this.creationDate = creationDate;
+    this.lastModifiedDate = lastModifiedDate;
+    this.countItems = countItems;
+
+    this.creator = creator;
+
+    if (!this.uid.length) {
+      const hash = crypto.createHash('md5').update(this.data).digest('hex');
+      this.uid = `${this.creator.uid}-${hash}`; //= > "local-useruid-7hy8hvrX"
+    }
   }
 
   static sequelize(client) {
@@ -16,6 +37,7 @@ class SearchQuery {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
+        field: 'id',
       },
       name: {
         type: DataTypes.STRING,
@@ -27,8 +49,38 @@ class SearchQuery {
         allowNull: false,
       },
       data: {
-        type: DataTypes.JSON,
+        type: DataTypes.STRING,
         allowNull: false,
+      },
+      creationDate: {
+        type: DataTypes.DATE,
+        field: 'date_created',
+        defaultValue: DataTypes.NOW,
+      },
+      lastModifiedDate: {
+        type: DataTypes.DATE,
+        field: 'date_last_modified',
+        defaultValue: DataTypes.NOW,
+      },
+      countItems: {
+        type: DataTypes.INTEGER,
+        field: 'count_items',
+        defaultValue: 0,
+      },
+      creatorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'creator_id',
+      },
+    }, {
+      tableName: 'search_queries',
+      defaultScope: {
+        include: [
+          {
+            model: creator,
+            as: 'creator',
+          },
+        ],
       },
     });
 
@@ -39,6 +91,26 @@ class SearchQuery {
       },
       onDelete: 'CASCADE',
     });
+
+    searchQuery.prototype.toJSON = function (obfuscate = true) {
+      const sq = new SearchQuery({
+        uid: this.uid,
+        name: this.name,
+        data: this.data,
+        description: this.description,
+        creationDate: this.creationDate,
+        lastModifiedDate: this.lastModifiedDate,
+        countItems: this.countItems,
+      });
+
+      if (this.creator) {
+        sq.creator = this.creator.toJSON({
+          obfuscate,
+        });
+      }
+      return sq;
+    };
+
     return searchQuery;
   }
 }
