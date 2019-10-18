1c2859909fb85ffeb551b7ae8707992729dc33b0
Daniele Guido <gui.daniele@gmail.com>
2019-10-18 11:16:00 +0200

    fix media service, remove passport lib

--- src/services/media.js
+++ src/services/media.js
@@ -2,14 +2,16 @@
 /* eslint import/no-dynamic-require: "off" */
 const debug = require('debug')('impresso/media');
 const { BadRequest, NotFound, NotAuthenticated } = require('@feathersjs/errors');
+const { authenticate } = require('@feathersjs/express');
 
 module.exports = function (app) {
   const config = app.get('media');
-  const authentication = app.get('authentication');
 
   debug('configuring media ...', config.host, config.path);
 
   app.use(`${config.path}/:service/:id`, [
+    // authenticate token!
+    authenticate('jwt'),
     function (req, res, next) {
       if (config.services.indexOf(req.params.service) === -1) {
         throw new BadRequest('incomplete request params (service)');
@@ -18,64 +20,26 @@ module.exports = function (app) {
       if (!/^\d+$/.test(req.params.id)) {
         throw new BadRequest('incomplete request params (id)');
       }
-      next();
-    },
-    function (req, res, next) {
-      debug('using req params:', req.params);
-      let accessToken = req.headers.authorization;
 
-      if (req.cookies && req.cookies[authentication.cookie.name]) {
-        accessToken = req.cookies[authentication.cookie.name] || req.headers.authorization;
-      }
-
-      if (!accessToken) {
-        debug('no auth found, return public contents only.');
-        // do nothing, we're going for the "public" endpoint
-        res.send('fin');
-        res.end();
-      } else {
-        res.locals.accessToken = accessToken.replace(/Bearer\s+/, '');
-        next();
-      }
-    },
-    function (req, res, next) {
-      // check payload
-      debug('check access token...');
-      app.passport.verifyJWT(res.locals.accessToken, {
-        secret: authentication.secret,
-      }).then((payload) => {
-        debug('access token valid, adding payloads...');
-        res.locals.payload = payload;
-        res.locals.user = {
-          uid: res.locals.payload.userId,
-        };
-        next();
-      }).catch((err) => {
-        debug('Error! auth found, with INVALID payload.', err);
-        next(new NotAuthenticated());
-      });
-    },
-    // get item according to service. item must have an attachment property
-    function (req, res, next) {
-      debug(`calling ${req.params.service}:get(${req.params.id}) with authentified user uid: ${res.locals.user.uid}`);
+      debug(`[${req.params.service}:${req.params.id}] Call ${req.params.service}.get using user uid: ${req.user.uid}`);
       // a class having an attachment
       app.service(req.params.service).get(req.params.id, {
-        user: res.locals.user,
+        user: req.user,
       }).then((item) => {
         res.locals.item = item;
-        debug(`service '${req.params.service}.get(${req.params.id})' success, check attachments...`);
+        debug(`[${req.params.service}:${req.params.id}]  ${req.params.service}.get success, check attachments...`);
+        if (!item.attachment) {
+          throw new NotFound();
+        }
         next();
       }).catch((err) => {
         next(err);
       });
     },
     function (req, res) {
-      if (!res.locals.item.attachment) {
-        throw new NotFound();
-      }
       const filename = res.locals.item.attachment.path.split('/').pop();
       const protectedFilepath = [config.protectedPath, res.locals.item.attachment.path].join('/');
-      debug('flush headers for filename:', filename, protectedFilepath);
+      debug(`[${req.params.service}:${req.params.id}]`, 'flush headers for filename:', filename, protectedFilepath);
       res.set('Content-Disposition', `attachment; filename=${filename}`);
       res.set('X-Accel-Redirect', protectedFilepath);
       res.send();
