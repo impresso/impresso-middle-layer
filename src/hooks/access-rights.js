e46759f22580afd9e0b6895994e40353ac321360
Daniele Guido <gui.daniele@gmail.com>
2019-11-04 14:33:02 +0100

    refactor hooks to obfuscate IIIF and contents

--- src/hooks/access-rights.js
+++ src/hooks/access-rights.js
@@ -5,6 +5,28 @@ const { ACCESS_RIGHTS_CLOSED } = require('../models/issues.model');
 debug('init hook config:', config.accessRights);
 
 /**
+ * @param  { Page } page without obfuscation
+ * @return { Page }      obfuscated iiif
+ */
+const obfuscatePageMapper = (page) => {
+  page.iiif = config.accessRights.unauthorizedIIIFUrl;
+  page.iiifThumbnail = config.accessRights.unauthorizedIIIFImageUrl;
+  page.obfuscated = true;
+  return page;
+};
+
+/**
+ * Gibven an issue instance, obfuscate iiif for any page
+ * @param  {[type]} issue [description]
+ * @return {[type]}       [description]
+ */
+const obfuscateIssueMapper = (issue) => {
+  issue.pages = issue.pages.map(obfuscatePageMapper);
+  issue.obfuscated = true;
+  return issue;
+};
+
+/**
  * Given an article, check the access rights of the related issue;
  * delete the excerpt and the content if the user has not signed an NDA or
  * is anonymous.
@@ -18,6 +40,7 @@ const obfuscateArticleMapper = (article) => {
   }
   if (config.accessRights.enable) {
     if (article.issue.accessRights === ACCESS_RIGHTS_CLOSED) {
+      article.issue.obfuscated = true;
       article.obfuscated = true;
       article.content = '... *** ...';
       article.regions = article.regions.map(d => ({
@@ -38,33 +61,37 @@ const obfuscateArticleMapper = (article) => {
   return article;
 };
 
-/**
- * Given a result of articles, check the access rights of the related issue;
- * delete the excerpt and the content if the user has not signed an NDA or
- * is anonymous.
- *
- * @param  { context } context from results
- * @return { Function }         hook function
- */
 const obfuscate = () => (context) => {
-  if (!context.result) {
-    throw new Error('The \'obfuscate\' hook should be used as a final hook.');
-  }
-  // should the content be obfuscated?
-  context.obfuscated = !context.params.authenticated;
+  const fullpath = `${context.path}.${context.method}`;
+  const prefix = `[obfuscate (${fullpath})]`;
 
-  if (context.obfuscated) {
-    // NOT authenticated? let's obfuscate if the related issue has access-right: closed
-    if (context.result.data) {
-      for (let i = 0, l = context.result.data.length; i < l; i += 1) {
-        if (context.result.data[i].issue.accessRights === ACCESS_RIGHTS_CLOSED) {
-          context.result.data[i] = obfuscateArticleMapper(context.result.data[i]);
+  if (context.params.authenticated) {
+    debug(`${prefix} skipping obfuscation as the user ${context.params.user.uid} has the right credentials`);
+  } else {
+    switch (fullpath) {
+      case 'issues.get':
+        if (context.result.accessRights === ACCESS_RIGHTS_CLOSED) {
+          debug(`${prefix} issue obfuscated due to context.result.accessRights: ${context.result.accessRights}`);
+          context.result = obfuscateIssueMapper(context.result);
         }
-      }
-      debug('context result data obfuscated.');
-    } else if (context.result.issue.accessRights === ACCESS_RIGHTS_CLOSED) {
-      context.result = obfuscateArticleMapper(context.result);
-      debug(`context result for id ${context.result.uid} obfuscated.`);
+        break;
+      case 'articles.get':
+        if (context.result.issue.accessRights === ACCESS_RIGHTS_CLOSED) {
+          debug(`${prefix} issue obfuscated due to context.result.issue.accessRights: ${context.result.issue.accessRights}`);
+          context.result = obfuscateArticleMapper(context.result);
+        }
+        break;
+      case 'articles.find':
+        debug(`${prefix} verify accessRights per article issue`);
+        for (let i = 0, l = context.result.data.length; i < l; i += 1) {
+          if (context.result.data[i].issue.accessRights === ACCESS_RIGHTS_CLOSED) {
+            context.result.data[i] = obfuscateArticleMapper(context.result.data[i]);
+          }
+        }
+        break;
+      default:
+        debug(`${prefix} WARNING no fullpath rule matching: '${fullpath}'`);
+        throw new Error(`${prefix} cannot use 'obfuscate()' on this service.`);
     }
   }
 };
