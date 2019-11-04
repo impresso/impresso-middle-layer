95597a3186ab9245861ad814e71274556787d46d
Daniele Guido <gui.daniele@gmail.com>
2019-11-04 15:31:39 +0100

    add issue to article find method

--- src/services/articles/articles.class.js
+++ src/services/articles/articles.class.js
@@ -1,4 +1,4 @@
-const lodash = require('lodash');
+const { keyBy } = require('lodash');
 const debug = require('debug')('impresso/services:articles');
 const { Op } = require('sequelize');
 const { NotFound } = require('@feathersjs/errors');
@@ -6,6 +6,7 @@ const { NotFound } = require('@feathersjs/errors');
 const SequelizeService = require('../sequelize.service');
 const SolrService = require('../solr.service');
 const Article = require('../../models/articles.model');
+const Issue = require('../../models/issues.model');
 
 class Service {
   constructor({
@@ -41,7 +42,7 @@ class Service {
       fl = Article.ARTICLE_SOLR_FL;
     }
 
-    debug(`'find' ${this.name} user:`, params.user ? params.user.uid : 'no user');
+    debug('[find] use auth user:', params.user ? params.user.uid : 'no user');
     // if(params.isSafe query.filters)
     const results = await this.SolrService.find({
       ...params,
@@ -53,9 +54,8 @@ class Service {
       return results;
     }
 
-    // add newspapers and other things from sequelize
-    // to be moved to the toBeResolved hook? proper hook.
-    const addons = await this.SequelizeService.find({
+    // add newspapers and other things from this class sequelize method
+    const getAddons = this.SequelizeService.find({
       ...params,
       scope: 'get',
       where: {
@@ -66,31 +66,44 @@ class Service {
     }).catch((err) => {
       console.error(err);
       return { data: [] };
-    });
+    }).then(({ data }) => keyBy(data, 'uid'));
 
-    // idnexed by article uid;
-    const addonsIndex = lodash.keyBy(addons.data, 'uid');
-    debug('addons found for:', Object.keys(addonsIndex));
-    results.data = results.data.map((article) => {
-      const addon = addonsIndex[article.uid];
+    // get accessRights from issues table
+    const getRelatedIssues = Issue.sequelize(this.app.get('sequelizeClient'))
+      .findAll({
+        attributes: [
+          'accessRights', 'uid',
+        ],
+        where: {
+          uid: { [Op.in]: results.data.map(d => d.issue.uid) },
+        },
+      }).then(rows => keyBy(rows.map(d => d.get()), 'uid'));
 
-      if (!addon) {
-        debug('no addons for uid', article.uid);
+    // do the loop
+    return Promise.all([
+      getAddons,
+      getRelatedIssues,
+    ]).then(([addonsIndex, issuesIndex]) => ({
+      ...results,
+      data: results.data.map((article) => {
+        if (issuesIndex[article.issue.uid]) {
+          article.issue.accessRights = issuesIndex[article.issue.uid].accessRights;
+        }
+        if (!addonsIndex[article.uid]) {
+          debug('[find] no pages for uid', article.uid);
+          return article;
+        }
+        // add pages
+        if (addonsIndex[article.uid].pages) {
+          article.pages = addonsIndex[article.uid].pages.map(d => d.toJSON());
+        }
+        if (pageUids.length === 1) {
+          article.regions = article.regions.filter(r => pageUids.indexOf(r.pageUid) !== -1);
+        }
+        article.assignIIIF();
         return article;
-      }
-
-      if (addon.pages) {
-        article.pages = addon.pages.map(d => d.toJSON());
-      }
-
-      if (pageUids.length === 1) {
-        article.regions = article.regions.filter(r => pageUids.indexOf(r.pageUid) !== -1);
-      }
-      article.assignIIIF();
-      return article;
-    });
-
-    return results;
+      }),
+    }));
   }
 
   async get(id, params) {
