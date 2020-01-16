90ec51fe157a773e31dbca252b3bf5a3826dad8c
theorm <theorm@gmail.com>
2020-01-16 16:17:30 +0100

    Removed unused params

--- src/services/ngram-trends/ngram-trends.class.js
+++ src/services/ngram-trends/ngram-trends.class.js
@@ -8,7 +8,7 @@ class NgramTrends {
     this.solrClient = app.get('solrClient');
   }
 
-  async create({ ngrams, filters, facets = [] }, params) {
+  async create({ ngrams, filters, facets = [] }) {
     const requestPayload = unigramTrendsRequestToSolrQuery(ngrams[0], filters, facets);
     const solrResponse = await this.solrClient.requestPostRaw(requestPayload);
     const response = await parseUnigramTrendsResponse(solrResponse);
