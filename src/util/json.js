82f5e2e7acc49930c83c706fae27bab03f73e810
theorm <theorm@gmail.com>
2020-01-22 15:09:27 +0100

    Added schema

--- src/util/json.js
+++ src/util/json.js
@@ -8,6 +8,8 @@ ajv.addSchema(require('../schema/search/filter.json'));
 // models
 ajv.addSchema(require('../schema/models/base-user.model.json'));
 ajv.addSchema(require('../schema/models/search-query.model.json'));
+ajv.addSchema(require('../schema/models/text-reuse/passage.json'));
+ajv.addSchema(require('../schema/models/text-reuse/cluster.json'));
 
 // services
 ajv.addSchema(require('../services/entities/schema/find/query.json'));
@@ -15,6 +17,7 @@ ajv.addSchema(require('../services/search-queries/schema/post/payload.json'));
 ajv.addSchema(require('../services/search-queries-comparison/schema/post/payload.json'));
 ajv.addSchema(require('../services/search-queries-comparison/schema/post/response.json'));
 ajv.addSchema(require('../services/newspapers/schema/find/query.json'));
+ajv.addSchema(require('../services/articles-text-reuse-passages/schema/find/response.json'));
 
 const BaseSchemaURI = 'https://github.com/impresso/impresso-middle-layer/tree/master/src';
 
