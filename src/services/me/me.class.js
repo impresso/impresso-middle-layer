5bce29893d3b39abf5a521ca0ebd949995c7220d
Daniele Guido <gui.daniele@gmail.com>
2020-02-06 16:36:10 +0100

    Feature/edit user profile (#232)
    * add custom modelname to services, default to service 'name'
    
    * retrieve user info from db

--- src/services/me/me.class.js
+++ src/services/me/me.class.js
@@ -1,11 +1,34 @@
 /* eslint-disable no-unused-vars */
+const debug = require('debug')('impresso/services:me');
+const SequelizeService = require('../sequelize.service');
+const User = require('../../models/users.model');
+
 class Service {
   constructor(options) {
     this.options = options || {};
   }
 
+  setup(app) {
+    this.app = app;
+    this.sequelizeService = new SequelizeService({
+      app,
+      name: 'me',
+      modelName: 'users',
+    });
+  }
+
   async find(params) {
-    return params.user;
+    const user = await this.sequelizeService.get(params.user.id, {});
+    debug('[find] retrieve current user:', user.profile.uid);
+    return {
+      email: user.email,
+      uid: user.profile.uid,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      creationDate: user.creationDate,
+      pattern: user.profile.pattern,
+      emailAccepted: user.profile.emailAccepted,
+    };
   }
 
   async update(id, data, params) {
