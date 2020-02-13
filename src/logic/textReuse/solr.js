1cff462b2a32caba4fd786d2844c4cb4df66532b
Roman Kalyakin <roman@kalyakin.com>
2020-02-13 13:36:27 +0100

    merged solr.js

---
+++ src/logic/textReuse/solr.js
@@ -0,0 +1,106 @@
+const assert = require('assert');
+const { get, has } = require('lodash');
+
+const PassageFields = {
+  Id: 'id',
+  ContentItemId: 'ci_id_s',
+  ClusterId: 'cluster_id_l',
+  OffsetStart: 'beg_offset_i',
+  OffsetEnd: 'end_offset_i',
+};
+
+const ClusterFields = {
+  Id: 'cluster_id_s',
+  LexicalOverlap: 'lex_overlap_d',
+  ContentItemsIds: 'member_id_full_ss',
+  TimeDifferenceDay: 'day_delta_f',
+};
+
+/**
+ * We assume there cannot be more than this many passages in an article.
+ */
+const DefaultPassagesLimit = 100;
+
+/**
+ * Get Solr query parameters for requesting text passages for an article.
+ * @param {string} articleId article ID
+ * @return {object} GET request query parameters
+ */
+function getTextReusePassagesRequestForArticle(articleId) {
+  assert.ok(typeof articleId === 'string' && articleId.length > 0, 'Article ID is required');
+  return {
+    q: `${PassageFields.ContentItemId}:${articleId}`,
+    hl: false,
+    rows: DefaultPassagesLimit,
+  };
+}
+
+/**
+ * Get Solr query parameters for requesting clusters by their Ids.
+ * @param {string[]} clusterIds Ids of clusters
+ * @return {object} GET request query parameters
+ */
+function getTextReuseClustersRequestForIds(clusterIds) {
+  assert.ok(Array.isArray(clusterIds) && clusterIds.length > 0, 'At least one cluster Id is required');
+  return {
+    q: clusterIds.map(clusterId => `${ClusterFields.Id}:${clusterId}`).join(' OR '),
+    hl: false,
+    rows: clusterIds.length,
+  };
+}
+
+function convertSolrPassageDocToPassage(doc) {
+  const [offsetStart, offsetEnd] = [
+    get(doc, PassageFields.OffsetStart),
+    get(doc, PassageFields.OffsetEnd),
+  ];
+
+  return {
+    id: get(doc, PassageFields.Id),
+    clusterId: has(doc, PassageFields.ClusterId)
+      ? String(get(doc, PassageFields.ClusterId)) : undefined,
+    offsetStart,
+    offsetEnd,
+  };
+}
+
+function convertPassagesSolrResponseToPassages(solrResponse) {
+  return get(solrResponse, 'response.docs', []).map(convertSolrPassageDocToPassage);
+}
+
+const ContentItemIdDateRegex = /^[^-]+-(\d{4}-\d{2}-\d{2})-.*$/;
+const extractDateFromContentItemId = (id) => {
+  const items = ContentItemIdDateRegex.exec(id);
+  if (items && items.length > 1) return new Date(items[1]);
+  return undefined;
+};
+const asDateString = date => date.toISOString().split('T')[0];
+
+function convertSolrClusterToCluster(doc) {
+  const ids = get(doc, ClusterFields.ContentItemsIds, []);
+  const dates = ids.map(extractDateFromContentItemId).sort((a, b) => a - b);
+  const [firstDate] = dates;
+  const timeDifferenceMs = get(doc, ClusterFields.TimeDifferenceDay) * 86400000;
+  const lastDate = new Date(firstDate.getTime() + timeDifferenceMs);
+
+  return {
+    id: get(doc, ClusterFields.Id),
+    lexicalOverlap: get(doc, ClusterFields.LexicalOverlap),
+    timeCoverage: {
+      from: asDateString(firstDate),
+      to: asDateString(lastDate),
+    },
+  };
+}
+
+function convertClustersSolrResponseToClusters(solrResponse) {
+  return get(solrResponse, 'response.docs', []).map(convertSolrClusterToCluster);
+}
+
+module.exports = {
+  getTextReusePassagesRequestForArticle,
+  convertPassagesSolrResponseToPassages,
+
+  getTextReuseClustersRequestForIds,
+  convertClustersSolrResponseToClusters,
+};
---
+++ src/services/articles-text-reuse-passages/articles-text-reuse-passages.class.js
@@ -0,0 +1,58 @@
+const { groupBy, mapValues, first } = require('lodash');
+const {
+  getTextReusePassagesRequestForArticle,
+  convertPassagesSolrResponseToPassages,
+
+  getTextReuseClustersRequestForIds,
+  convertClustersSolrResponseToClusters,
+} = require('../../logic/textReuse/solr');
+
+function buildResponse(passages, clusters) {
+  const clustersById = mapValues(groupBy(clusters, 'id'), first);
+  return {
+    passages: passages.map(({
+      id, clusterId, offsetStart, offsetEnd,
+    }) => {
+      const { lexicalOverlap, timeCoverage } = clustersById[clusterId];
+      return {
+        id,
+        clusterId,
+        lexicalOverlap,
+        timeCoverage,
+        offsetStart,
+        offsetEnd,
+      };
+    }),
+  };
+}
+
+class ArticlesTextReusePassages {
+  constructor(options, app) {
+    this.options = options || {};
+    this.solrClient = app.get('solrClient');
+  }
+
+  async find(params) {
+    const { articleId } = params.route;
+
+    // 1. Get passages and clusters
+    const passages = await this.solrClient
+      .requestGetRaw(getTextReusePassagesRequestForArticle(articleId), 'tr_passages')
+      .then(convertPassagesSolrResponseToPassages);
+    const clusterIds = [...new Set(passages.map(({ clusterId }) => clusterId))];
+    const clusters = clusterIds.length > 0
+      ? await this.solrClient
+        .requestGetRaw(getTextReuseClustersRequestForIds(clusterIds), 'tr_clusters')
+        .then(convertClustersSolrResponseToClusters)
+      : [];
+
+    // 2. Construct response
+    const response = buildResponse(passages, clusters);
+
+    return response;
+  }
+}
+
+module.exports = {
+  ArticlesTextReusePassages,
+};
