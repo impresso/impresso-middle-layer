927bf97e75db8b303470f619c08375b6d3c5c384
Daniele Guido <gui.daniele@gmail.com>
2020-02-18 16:06:22 +0100

    Feature/change user password (#244)
    * add validation for new password and previous password
    * remove regex validation for low secure password
    * add payload schema for user update

--- src/services/me/me.hooks.js
+++ src/services/me/me.hooks.js
@@ -1,5 +1,7 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 const { discard } = require('feathers-hooks-common');
+const { REGEX_PASSWORD, validate } = require('../../hooks/params');
+const { validateWithSchema } = require('../../hooks/schema');
 
 module.exports = {
   before: {
@@ -7,8 +9,38 @@ module.exports = {
     find: [],
     get: [],
     create: [],
-    update: [],
-    patch: [],
+    update: [
+      validateWithSchema('services/me/schema/post/payload.json'),
+      validate({
+        pattern: {
+          regex: /^#[a-f0-9]{2,6}$/,
+          after: d => d.join(','),
+        },
+        firstname: {
+          after: d => d.trim(),
+        },
+        lastname: {
+          after: d => d.trim(),
+        },
+        email: {
+          after: d => d.trim(),
+        },
+        displayName: {
+          after: d => d.trim(),
+        },
+      }, 'POST'),
+    ],
+    patch: [
+      validate({
+        previousPassword: {
+          required: false,
+        },
+        newPassword: {
+          required: false,
+          regex: REGEX_PASSWORD,
+        },
+      }, 'POST'),
+    ],
     remove: [],
   },
 
