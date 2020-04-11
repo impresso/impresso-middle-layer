b61109a6822287963b24f359251f3b120786f40f
Roman Kalyakin <roman@kalyakin.com>
2020-04-11 09:29:13 +0200

    create constrained solr query

--- src/services/search-queries-comparison/search-queries-comparison.class.js
+++ src/services/search-queries-comparison/search-queries-comparison.class.js
@@ -1,10 +1,7 @@
 // @ts-check
+
 // const debug = require('debug')('impresso/services:search-queries-comparison');
-const {
-  flatten, values, groupBy,
-  uniq, omitBy, isNil,
-} = require('lodash');
-const { BadRequest } = require('@feathersjs/errors')
+const { BadRequest } = require('@feathersjs/errors');
 const { logic: { filter: { mergeFilters } } } = require('impresso-jscommons');
 const { SolrMappings } = require('../../data/constants');
 
@@ -17,68 +14,66 @@ const { SolrMappings } = require('../../data/constants');
  */
 
 const {
-  getItemsFromSolrResponse,
   getFacetsFromSolrResponse,
-  getTotalFromSolrResponse,
 } = require('../search/search.extractors');
-const { sameTypeFiltersToQuery, filtersToQueryAndVariables } = require('../../util/solr');
-const { SolrNamespaces } = require('../../solr');
+const { filtersToQueryAndVariables } = require('../../util/solr');
 
-// TODO: Do we need to make it configurable in request?
-const DefaultSolrFieldsFilter = ['id', 'pp_plain:[json]', 'lg_s'].join(',');
 
 /**
- * Given comparison request (`https://github.com/impresso/impresso-middle-layer/tree/master/src/services/search-queries-comparison/schema/intersection/post/request.json`),
- * build queries intersection payload for `solrService.findAll`.
+ * Create SOLR query for getting facets.
+ * There are two types of facet sections in the query: simple and constrained.
  *
- * @param {object} request - comparison request.
- * @return {object} `solrService.findAll` payload.
- */
-function intersectionRequestToSolrQuery(request) {
-  const {
-    order_by: orderBy,
-    facets,
-    limit,
-    skip,
-    queries,
-  } = request;
-
-  const allFilters = flatten(queries.map(({ filters }) => filters));
-
-  const filtersGroupsByType = values(groupBy(allFilters, 'type'));
-  const solrQueries = uniq(filtersGroupsByType
-    .map(f => sameTypeFiltersToQuery(f, SolrNamespaces.Search)));
-
-  const q = solrQueries.join(' AND ');
-  const fl = DefaultSolrFieldsFilter;
-
-  return omitBy({
-    q,
-    order_by: orderBy,
-    facets,
-    limit,
-    skip,
-    fl,
-  }, isNil);
-}
-
-/**
+ * Simple type is a standard "term" facet for a field which returns N top items.
+ *
+ * Constrained type is created when for a certain field we need to retrieve counts
+ * of certain variables provided in buckets of a facet of particular type in `constraintFacets`.
+ * An example of a constrained facet section could be a "person" facet where we want to retrieve
+ * counts of documents that mention persons with IDs "A", "B" and "C". To achieve this we create
+ * a separate facet entry for every person ID. Such entry has a type "query" and a filter ("q")
+ * matching particular ID.
+ *
+ * See unit tests for examples.
+ *
  * @param {Filter[]} filters
  * @param {FacetRequest[]} facetsRequests
+ * @param {Facet[]} constraintFacets
  * @returns {any}
  */
-function createQuery(filters, facetsRequests) {
-  const { query, variables } = filtersToQueryAndVariables(filters);
+function createSolrQuery(filters, facetsRequests, constraintFacets = []) {
+  const { query } = filtersToQueryAndVariables(filters);
 
   const facets = facetsRequests.reduce((acc, { type, skip, limit }) => {
     const facet = SolrMappings.search.facets[type];
     if (facet == null) throw new BadRequest(`Unknown facet type: "${type}"`);
 
+    // 1. See if there is a constraint for this particular facet type.
+    const constraintFacet = constraintFacets.find(({ type: facetType }) => facetType === type);
+
+    // 2. If there is a constraint.
+    if (constraintFacet != null) {
+      // Go through every bucket in the constraint
+      return constraintFacet.buckets.reduce((facetAcc, { val }, index) => {
+        // Create a Solr query for this bucket's value.
+        const { query: constraintQuery } = filtersToQueryAndVariables([
+          { type, q: val },
+        ]);
+        // Create a constrained entry in the query.
+        return {
+          ...facetAcc,
+          [`constrained__${type}__${index}`]: {
+            type: 'query',
+            q: constraintQuery,
+          },
+        };
+      }, acc);
+    }
+
+    // 3. Otherwise create a standard facet entry.
     return {
       ...acc,
       [type]: {
         ...facet,
-        skip: skip == null ? facet.skip : skip,
+        offset: skip == null ? facet.skip : skip,
         limit: limit == null ? facet.limit : limit,
       },
     };
@@ -87,7 +82,7 @@ function createQuery(filters, facetsRequests) {
   return {
     query,
     limit: 0,
-    params: { hl: false, ...variables },
+    params: { hl: false },
     facet: facets,
   };
 }
@@ -134,12 +129,14 @@ class SearchQueriesComparison {
     const intersectionFilters = request.filtersSets
       .reduce((mergedFilters, filters) => mergeFilters(mergedFilters.concat(filters)));
 
-    const intersectionSolrQuery = createQuery(intersectionFilters, request.facets);
+    const intersectionSolrQuery = createSolrQuery(intersectionFilters, request.facets);
     const intersectionFacets = await this.solr
       .post(intersectionSolrQuery, this.solr.namespaces.Search)
       .then(getResponseFacetsFromSolrResponse);
 
-    console.log(intersectionSolrQuery, intersectionFacets, userInfo);
+    const queryA = createSolrQuery(request.filtersSets[0], request.facets, intersectionFacets);
+
+    console.log(intersectionSolrQuery, intersectionFacets, userInfo, queryA);
 
     return {
       facetsSets: request.filtersSets.map(() => request.facets
@@ -147,40 +144,9 @@ class SearchQueriesComparison {
       intersectionFacets,
     };
   }
-
-  /**
-   * Execute `intersection` search that returns results which match all provided queries.
-   * @param {object} request - comparison request
-   * @param {object} userInfo - a `{ user, authenticated }` object
-   * @return {object} response body
-   */
-  async findIntersectingItemsBetweenQueries(request, userInfo) {
-    const solrQuery = intersectionRequestToSolrQuery(request);
-    const response = await this.solrClient.findAll(solrQuery);
-
-    const data = await getItemsFromSolrResponse(response, this.articlesService, userInfo);
-    const facets = await getFacetsFromSolrResponse(response);
-    const total = getTotalFromSolrResponse(response);
-
-    const { limit, skip } = request;
-
-    return {
-      data,
-      limit,
-      skip,
-      total,
-      info: {
-        responseTime: {
-          solr: response.responseHeader.QTime,
-        },
-        facets,
-      },
-    };
-  }
 }
 
 module.exports = {
   SearchQueriesComparison,
-  intersectionRequestToSolrQuery,
-  DefaultSolrFieldsFilter,
+  createSolrQuery,
 };
--- test/services/search-queries-comparison.test.js
+++ test/services/search-queries-comparison.test.js
@@ -1,10 +1,15 @@
 const assert = require('assert');
 const app = require('../../src/app');
 const {
-  intersectionRequestToSolrQuery,
-  DefaultSolrFieldsFilter,
+  createSolrQuery,
 } = require('../../src/services/search-queries-comparison/search-queries-comparison.class');
-const hooks = require('../../src/services/search-queries-comparison/search-queries-comparison.hooks');
+
+/**
+ * @typedef {import('impresso-jscommons').Filter} Filter
+ * @typedef {import('impresso-jscommons').Facet} Facet
+ * @typedef {import('impresso-jscommons').Bucket} Bucket
+ * @typedef {import('../../src/services/search-queries-comparison').FacetRequest} FacetRequest
+ */
 
 describe('\'search-queries-comparison\' service', () => {
   it('registered the service', () => {
@@ -13,154 +18,76 @@ describe('\'search-queries-comparison\' service', () => {
   });
 });
 
-describe('intersectionRequestToSolrQuery', () => {
-  it('builds a basic query', () => {
-    const request = {
-      queries: [
-        {
-          filters: [
-            { type: 'person', q: ['Bonnie', 'Clyde'], op: 'AND' },
-          ],
-        },
-        {
-          filters: [
-            { type: 'newspaper', q: 'NYT' },
-          ],
-        },
-      ],
-    };
-    const expectedPayload = {
-      fl: DefaultSolrFieldsFilter,
-      q: 'filter(pers_entities_dpfs:Bonnie AND pers_entities_dpfs:Clyde) AND filter(meta_journal_s:NYT)',
-    };
-
-    const payload = intersectionRequestToSolrQuery(request);
-    assert.deepEqual(payload, expectedPayload);
-  });
-
-  it('builds a query with all parameters', () => {
-    const request = {
-      queries: [
-        {
-          filters: [
-            { type: 'collection', q: 'A' },
-          ],
-        },
-        {
-          filters: [
-            { type: 'collection', q: 'B' },
-          ],
-        },
-      ],
-      skip: 5,
-      limit: 3,
-      order_by: 'id',
-      facets: 'test',
-    };
-    const expectedPayload = {
-      fl: DefaultSolrFieldsFilter,
-      q: 'filter(ucoll_ss:A AND ucoll_ss:B)',
-      limit: 3,
-      skip: 5,
-      order_by: 'id',
-      facets: 'test',
-    };
-
-    const payload = intersectionRequestToSolrQuery(request);
-    assert.deepEqual(payload, expectedPayload);
-  });
-
-  it('fails to builds a query with wrong filter types', () => {
-    const request = {
-      queries: [
-        {
-          filters: [
-            { type: 'foobar', q: 'A' },
-          ],
-        },
-        {
-          filters: [
-            { type: 'collection', q: 'B' },
-          ],
-        },
-      ],
-    };
-
-    assert.throws(
-      () => intersectionRequestToSolrQuery(request),
-      {
-        message: 'reduceFilterToSolr: filter function for \'foobar\' not found',
-      },
-    );
-  });
-});
-
-describe('post hooks sequence', () => {
-  it('generates correct request for valid payload', async () => {
-    const context = {
+describe('createSolrQuery', () => {
+  it('creates a query without constraints', () => {
+    const filters = /** @type {Filter[]} */ [
+      { type: 'person', q: 'person-a-id' },
+    ];
+    const facetRequests = /** @type {FacetRequest[]} */ [
+      { type: 'person', limit: 3, skip: 5 },
+    ];
+    const expectedRequest = {
+      limit: 0,
       params: {
-        query: {
-          method: 'intersection',
+        hl: false,
+      },
+      query: 'filter(pers_entities_dpfs:person-a-id)',
+      facet: {
+        person: {
+          field: 'pers_entities_dpfs',
+          limit: 3,
+          offset: 5,
+          type: 'terms',
+          mincount: 1,
+          numBuckets: true,
         },
       },
-      data: {
-        queries: [
+    };
+
+    const request = createSolrQuery(filters, facetRequests);
+    assert.deepEqual(request, expectedRequest);
+  });
+
+  it('creates a query with constraints', () => {
+    const filters = /** @type {Filter[]} */ [
+      { type: 'person', q: 'person-a-id' },
+    ];
+    const facetRequests = /** @type {FacetRequest[]} */ [
+      { type: 'person', limit: 3, skip: 5 },
+    ];
+    const facetConstraints = /** @type {Facet[]} */ [
+      {
+        type: 'person',
+        buckets: [
           {
-            filters: [{ type: 'topic', q: 'AA' }],
+            val: 'person-b-id',
           },
           {
-            filters: [{ type: 'topic', q: 'BB' }],
+            val: 'person-c-id',
           },
         ],
       },
-    };
+    ];
 
-    await Promise.all(hooks.before.create.map(async (hook) => {
-      await hook(context);
-      return context;
-    }));
-
-    const expectedPayload = {
-      limit: 10,
-      max_limit: 1000,
-      skip: 0,
-      queries: [
-        {
-          filters: [{
-            type: 'topic', q: 'AA', op: 'OR', context: 'include',
-          }],
-        },
-        {
-          filters: [{
-            type: 'topic', q: 'BB', op: 'OR', context: 'include',
-          }],
-        },
-      ],
-    };
-    assert.deepEqual(context.data.sanitized, expectedPayload);
-  });
-
-  it('throws "BadRequest" when payload does not match schema', async () => {
-    const context = {
+    const expectedRequest = {
+      limit: 0,
       params: {
-        query: {
-          method: 'intersection',
-        },
+        hl: false,
       },
-      data: {
-        queries: [],
+      query: 'filter(pers_entities_dpfs:person-a-id)',
+      facet: {
+        constrained__person__0: {
+          type: 'query',
+          q: 'filter(pers_entities_dpfs:person-b-id)',
+        },
+        constrained__person__1: {
+          type: 'query',
+          q: 'filter(pers_entities_dpfs:person-c-id)',
+        },
       },
     };
 
-    await assert.rejects(
-      Promise.all(hooks.before.create.map(async (hook) => {
-        await hook(context);
-        return context;
-      })),
-      {
-        code: 400,
-        message: 'JSON validation errors: data.queries should NOT have fewer than 2 items',
-      },
-    );
+    const request = createSolrQuery(filters, facetRequests, facetConstraints);
+    assert.deepEqual(request, expectedRequest);
   });
 });
