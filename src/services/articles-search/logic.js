6fa827c3180ee16e5682b88292582a0bc66b20da
Roman Kalyakin <roman@kalyakin.com>
2020-04-20 19:46:44 +0200

    formula builder for the other type

--- src/services/articles-search/logic.js
+++ src/services/articles-search/logic.js
@@ -4,6 +4,7 @@
  * @typedef {import('.').RelevanceContextItem} RelevanceContextItem
  * @typedef {import('.').RelevanceContextItemType} RelevanceContextItemType
  * @typedef {import('.').TimeRangeContextParameters} TimeRangeContextParameters
+ * @typedef {import('.').ItemContextParameters} ItemContextParameters
  */
 
 const RelevanceContextItemTypes = Object.freeze({
@@ -11,6 +12,14 @@ const RelevanceContextItemTypes = Object.freeze({
   Locations: /** @type {RelevanceContextItemType} */ ('locations'),
   Persons: /** @type {RelevanceContextItemType} */ ('persons'),
   Topics: /** @type {RelevanceContextItemType} */ ('topics'),
+});
+
+/** @type {{ [key: string]: string }} */
+const ContextTypeSolrFields = Object.freeze({
+  timeRange: 'meta_year_i',
+  locations: 'loc_entities_dpfs',
+  persons: 'pers_entities_dpfs',
+  topics: 'topics_dpfs',
 });
 
 /**
@@ -50,6 +59,28 @@ function timeRangeFormula({ startYear, endYear }) {
 }
 
 /**
+ * @param {RelevanceContextItemType} type
+ * @param {ItemContextParameters} itemContextParameters
+ * @returns {string}
+ */
+function itemContextFormula(type, { entities }) {
+  const solrField = ContextTypeSolrFields[type];
+  const items = entities.map(entity => `
+    mul(
+      payload(${solrField},${entity.id}),
+      ${entity.weight}
+    )`);
+
+  if (items.length > 1) {
+    return `sum(${items.join(',')})`;
+  }
+  if (items.length === 1) {
+    return items[0];
+  }
+  return '1.0';
+}
+
+/**
  *
  * @param {RelevanceContextItem} relevanceContextItem
  * @returns {string}
@@ -59,6 +90,8 @@ function relevanceContextItemToSolrFormula({ type, parameters, weight }) {
 
   if (type === RelevanceContextItemTypes.TimeRange) {
     parametersFormula = timeRangeFormula(/** @type {TimeRangeContextParameters} */ (parameters));
+  } else {
+    parametersFormula = itemContextFormula(type, /** @type {ItemContextParameters} */ (parameters));
   }
   return `mul(${parametersFormula},${weight})`.replace(/[\s\n]/g, '');
 }
