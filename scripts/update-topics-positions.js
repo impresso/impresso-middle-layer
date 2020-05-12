cf236ca83a43e83c6f3bc0043eefe4e0b474abd8
Daniele Guido <gui.daniele@gmail.com>
2020-05-12 10:16:15 +0200

    add hubs and authorities

--- scripts/update-topics-positions.js
+++ scripts/update-topics-positions.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const { groupBy } = require('lodash');
 const debug = require('debug')('impresso/scripts:update-topics-positions');
 const Graph = require('graphology');
 const forceAtlas2 = require('graphology-layout-forceatlas2');
 const pagerank = require('graphology-pagerank');
 const louvain = require('graphology-communities-louvain');
+const hits = require('graphology-hits');
 const { circular } = require('graphology-layout');
 const topics = require('../data/topics.json');
 
@@ -43,25 +45,33 @@ if (!x && !y) {
 }
 
 const positions = forceAtlas2(graph, {
-  iterations: 5000,
+  iterations: 100,
   settings: {
-    gravity: 50,
+    gravity: 20,
     linLogMode: false,
   },
 });
 
 const pageranks = pagerank(graph, { alpha: 0.9, weighted: true });
 const communities = louvain(graph);
+const { hubs, authorities } = hits(graph, { normalize: false });
+// const degreesPerCommunity = groupBy(communities, 'uid');
 
 debug('positions n.', Object.keys(pageranks).length);
 debug('pageranks n.', Object.keys(pageranks).length);
 debug('communities n.', Object.keys(communities).length);
+debug('hubs n.', Object.keys(hubs).length);
+debug('authorities n.', Object.keys(authorities).length);
+
 
 Object.keys(positions).forEach((uid) => {
   topics[uid].x = positions[uid].x;
   topics[uid].y = positions[uid].y;
   topics[uid].pagerank = pageranks[uid];
   topics[uid].community = communities[uid];
+  topics[uid].hub = hubs[uid];
+  topics[uid].authority = authorities[uid];
+
   debug(
     'topic', uid,
     '- x y:', topics[uid].x, topics[uid].y,
