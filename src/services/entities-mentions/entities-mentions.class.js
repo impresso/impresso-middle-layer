7d22c87901acd9bde4bf752c1546a63eb8c180ee
Roman Kalyakin <roman@kalyakin.com>
2020-07-26 09:58:25 +0200

    updated fields names and get entities types from db

--- src/services/entities-mentions/entities-mentions.class.js
+++ src/services/entities-mentions/entities-mentions.class.js
@@ -2,8 +2,15 @@
 const {
   uniq, values, groupBy, get,
 } = require('lodash');
+const { QueryTypes } = require('sequelize');
 const { SolrNamespaces } = require('../../solr');
 const { sameTypeFiltersToQuery } = require('../../util/solr');
+const { getCacheKeyForReadSqlRequest } = require('../sequelize.service');
+const { measureTime } = require('../../util/instruments');
+
+const QueryGetEntityTypeMap = `SELECT id, name
+  FROM meta_entities
+  WHERE prefix="meta_named entity"`;
 
 /**
  * @typedef {import('../../models').Filter} Filter
@@ -12,12 +19,8 @@ const { sameTypeFiltersToQuery } = require('../../util/solr');
 const Fields = Object.freeze({
   EntityId: 'e_id_s',
   WikidataId: 'wkd_id_s',
-  Name: 'm_name_s',
+  Name: 'm_name_text',
   TypeCode: 'm_type_s',
-});
-
-const TypeCodeToEntityType = Object.freeze({
-  50: 'Person',
 });
 
 function filtersToSolrQuery(filters) {
@@ -45,22 +48,22 @@ function buildSolrQuery(filters, limit = 10, skip = 0) {
   };
 }
 
-function documentToEntitOrMention(doc) {
+function documentToEntitOrMention(doc, entityTypeMapping) {
   return {
     type: doc[Fields.WikidataId] == null ? 'mention' : 'entity',
     id: doc[Fields.EntityId],
     label: doc[Fields.Name],
-    entityType: TypeCodeToEntityType[doc[Fields.TypeCode]],
+    entityType: entityTypeMapping[doc[Fields.TypeCode]],
     wikidataId: doc[Fields.WikidataId],
   };
 }
 
-function parseSolrResponse(response) {
+function parseSolrResponse(response, entityTypeMapping) {
   const total = get(response, `grouped.${Fields.EntityId}.matches`, 0);
   const docs = get(response, `grouped.${Fields.EntityId}.doclist.docs`, []);
   return {
     total,
-    items: docs.map(documentToEntitOrMention),
+    items: docs.map(doc => documentToEntitOrMention(doc, entityTypeMapping)),
   };
 }
 
@@ -68,6 +71,26 @@ class EntitiesMentions {
   constructor(app) {
     /** @type {import('../../cachedSolr').CachedSolrClient} */
     this.solr = app.get('cachedSolr');
+    /** @type {import('sequelize').Sequelize} */
+    this.sequelize = app.get('sequelizeClient');
+    /** @type {import('cache-manager').Cache} */
+    this.cacheManager = app.get('cacheManager');
+  }
+
+  /**
+   * @returns {Promise<{[key: string]: string}>}
+   */
+  async getEntityTypeByCodeMap() {
+    const cacheKey = getCacheKeyForReadSqlRequest(QueryGetEntityTypeMap);
+    const result = await measureTime(() => this.cacheManager.wrap(
+      cacheKey,
+      () => this.sequelize.query(QueryGetEntityTypeMap, { type: QueryTypes.SELECT }),
+    ), 'entities-mentions.find.db.entities_types');
+
+    return Object.freeze(result.reduce((acc, row) => {
+      acc[row.id] = row.name;
+      return acc;
+    }, {}));
   }
 
   /**
@@ -75,9 +98,11 @@ class EntitiesMentions {
    * @param {{ filters: Filter[], limit?: number, skip?: number }} body
    */
   async create({ filters, limit = 10, skip = 0 }) {
+    const entityTypeMapping = await this.getEntityTypeByCodeMap();
+
     const query = buildSolrQuery(filters, limit, skip);
     const solrResponse = await this.solr.post(query, this.solr.namespaces.EntitiesMentions);
-    const response = parseSolrResponse(solrResponse);
+    const response = parseSolrResponse(solrResponse, entityTypeMapping);
 
     response.skip = skip;
     response.limit = limit;
