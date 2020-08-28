6e73f291cdb791704c7517895644af4a193c1499
Roman Kalyakin <roman@kalyakin.com>
2020-08-28 12:24:59 +0200

    fixed daterange parsing

--- src/services/filters-items/extractors.js
+++ src/services/filters-items/extractors.js
@@ -3,14 +3,24 @@ const Topic = require('../../models/topics.model');
 const Entity = require('../../models/entities.model');
 const Year = require('../../models/years.model');
 
+const isDateRangeString = v => v.match(/.+ TO .+/) != null;
+const getDateStrings = v => v.match(/(.+) TO (.+)/).slice(1, 3);
 
 function daterangeExtractor({ q = '' }) {
-  const [start, end] = Array.isArray(q)
-    ? q
-    : q.trim().split(' TO ');
-  return start && end
-    ? [{ start, end }]
-    : [];
+  const values = Array.isArray(q) ? q : [q];
+
+  // if `q` is an array with two date strings, return one item for them
+  const isTwoDatesArray = values.length === 2 && values.filter(isDateRangeString).length === 0;
+  if (isTwoDatesArray) {
+    const [start, end] = values;
+    return [{ start, end }];
+  }
+
+  // otherwise parse ranges
+  return values.map((value) => {
+    const [start, end] = getDateStrings(value);
+    return { start, end };
+  });
 }
 
 function newspaperExtractor({ q = '' }) {
