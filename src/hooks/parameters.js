bd5a9c8b978c90eb7bed288c4799fb7eb87b2b00
Daniele Guido <gui.daniele@gmail.com>
2023-01-19 16:05:50 +0100

    Create parameters.js

---
+++ src/hooks/parameters.js
@@ -0,0 +1,39 @@
+const { BadRequest } = require('@feathersjs/errors')
+
+/**
+ * Please call this hook after id has been validated with the `validateId` hook
+ */
+const splitId =
+  (separator = ',', againstList = []) =>
+  async (context) => {
+    if (Array.isArray(context.id)) {
+      return context
+    }
+    context.id = context.id.split(separator)
+    if (
+      againstList.length > 0 &&
+      context.id.some((d) => !againstList.includes(d))
+    ) {
+      throw new BadRequest(
+        `id param should be one of these values: ${JSON.stringify(
+          againstList
+        )}, found: "${context.id}"`
+      )
+    }
+    return context
+  }
+
+const limitNumberOfIds =
+  (limit = 0) =>
+  (context) => {
+    if (!Array.isArray(context.id)) {
+      return context
+    }
+    if (context.id.length > limit) {
+      throw new BadRequest(
+        `id param must be a comma separated list of max ${limit} items, found: "${context.id}" (${context.id.length} items)`
+      )
+    }
+  }
+
+module.exports = { splitId, limitNumberOfIds }
