16993d91b9b87eb11c8f2a41870bd15068304d14
Daniele Guido <gui.daniele@gmail.com>
2023-04-03 18:22:03 +0200

    add StatsToSolrStatistics constant in stats common module

--- src/services/stats/common.js
+++ src/services/stats/common.js
@@ -1,22 +1,34 @@
-
-const TimeDomain = 'time';
+const TimeDomain = 'time'
 
 const StatsToSolrFunction = {
-  stddev: field => `stddev(${field})`,
-  mean: field => `avg(${field})`,
-  min: field => `min(${field})`,
-  max: field => `max(${field})`,
-  p99_7: field => `percentile(${field}, 99.7)`,
-  p50: field => `percentile(${field}, 50)`,
-  p68: field => `percentile(${field}, 68)`,
-};
+  stddev: (field) => `stddev(${field})`,
+  mean: (field) => `avg(${field})`,
+  min: (field) => `min(${field})`,
+  max: (field) => `max(${field})`,
+  p99_7: (field) => `percentile(${field}, 99.7)`,
+  p50: (field) => `percentile(${field}, 50)`,
+  p68: (field) => `percentile(${field}, 68)`,
+}
 
-const SupportedStats = Object.freeze(Object.keys(StatsToSolrFunction));
-const DefaultStats = ['stddev', 'min', 'max', 'mean', 'p99_7'];
+const StatsToSolrStatistics = {
+  stddev: 'stddev=true',
+  min: 'min=true',
+  max: 'max=true',
+  mean: 'mean=true',
+  p99_7: 'percentiles="99.7"',
+  p50: 'percentiles="50"',
+  p68: 'percentiles="68"',
+}
+
+const SupportedStats = Object.freeze(Object.keys(StatsToSolrFunction))
+const SupportedStatistics = Object.freeze(Object.keys(StatsToSolrStatistics))
+const DefaultStats = ['stddev', 'min', 'max', 'mean', 'p99_7']
 
 module.exports = {
   TimeDomain,
   StatsToSolrFunction,
+  StatsToSolrStatistics,
   SupportedStats,
+  SupportedStatistics,
   DefaultStats,
-};
+}
