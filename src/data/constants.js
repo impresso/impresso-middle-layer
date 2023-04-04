5b63d3538f2587dba0088f7a9380b161baae7cdb
Daniele Guido <gui.daniele@gmail.com>
2023-04-04 14:23:10 +0200

    eslint fix

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,13 +4,13 @@ const { DataIndex } = require('./index')
 
 const facetRanges = new DataIndex({ name: 'facetRanges' })
 
-function getRangeFacetValue(index, facet, key, defaultValue) {
+function getRangeFacetValue (index, facet, key, defaultValue) {
   const indexData = facetRanges.getValue(index) || {}
   const { [facet]: descriptor = {} } = indexData
   return descriptor[key] == null ? defaultValue : descriptor[key]
 }
 
-function getRangeFacetParametersWithDefault(
+function getRangeFacetParametersWithDefault (
   index,
   facet,
   numBuckets,
@@ -332,13 +332,6 @@ const SolrMappings = Object.freeze({
       country: {
         type: 'terms',
         field: 'meta_country_code_s',
-        mincount: 1,
-        limit: 10,
-        numBuckets: true,
-      },
-      type: {
-        type: 'terms',
-        field: 'item_type_s',
         mincount: 1,
         limit: 10,
         numBuckets: true,
