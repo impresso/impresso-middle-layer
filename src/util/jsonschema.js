211e7650ac2f55475a49bdfacd1cb62d096a3417
Roman Kalyakin <roman@kalyakin.com>
2024-03-29 09:00:53 +0100

    update ajv regex validation

--- src/util/jsonschema.js
+++ src/util/jsonschema.js
@@ -1,32 +1,27 @@
-const Ajv = require('ajv')
+const Ajv = require('ajv');
 
-const ajv = new Ajv({ allErrors: true, strictKeywords: true })
-const BaseSchemaURI =
-  'https://github.com/impresso/impresso-public-api/tree/master/src/schema'
+const ajv = new Ajv({ allErrors: true, strict: true });
+const BaseSchemaURI = 'https://github.com/impresso/impresso-public-api/tree/master/src/schema';
 
-ajv.addSchema(require('../schema/filter.json'))
-ajv.addSchema(require('../schema/filters.json'))
-ajv.addSchema(require('../schema/find.json'))
-ajv.addSchema(require('../schema/addons.json'))
+ajv.addSchema(require('../schema/filter.json'));
+ajv.addSchema(require('../schema/filters.json'));
+ajv.addSchema(require('../schema/find.json'));
+ajv.addSchema(require('../schema/addons.json'));
 
-function validated (obj, schemaUri) {
-  const uri = schemaUri.startsWith('http')
-    ? schemaUri
-    : `${BaseSchemaURI}/${schemaUri}`
-  const validate = ajv.getSchema(uri)
+function validated(obj, schemaUri) {
+  const uri = schemaUri.startsWith('http') ? schemaUri : `${BaseSchemaURI}/${schemaUri}`;
+  const validate = ajv.getSchema(uri);
 
   if (validate === undefined) {
-    throw new Error(`No such schema found: ${uri}`)
+    throw new Error(`No such schema found: ${uri}`);
   }
-  const isValid = validate(obj)
+  const isValid = validate(obj);
   if (!isValid) {
-    const error = new Error(
-      `JSON validation errors: ${ajv.errorsText(validate.errors)}`
-    )
-    error.errors = validate.errors
-    throw error
+    const error = new Error(`JSON validation errors: ${ajv.errorsText(validate.errors)}`);
+    error.errors = validate.errors;
+    throw error;
   }
-  return obj
+  return obj;
 }
 
 /**
@@ -37,18 +32,18 @@ function validated (obj, schemaUri) {
  * @param {Array} errors
  * @returns {Object}
  */
-function formatValidationErrors (errors = []) {
+function formatValidationErrors(errors = []) {
   return errors.reduce((acc, error) => {
     if (!Array.isArray(acc[error.keyword])) {
-      acc[error.keyword] = []
+      acc[error.keyword] = [];
     }
-    acc[error.keyword].push(error)
+    acc[error.keyword].push(error);
     // acc[error.keyword]
-    return acc
-  }, {})
+    return acc;
+  }, {});
 }
 
 module.exports = {
   validated,
   formatValidationErrors,
-}
+};
