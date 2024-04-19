543bf7ae838495d003de339d7bb1f9c760ac2593
Roman Kalyakin <roman@kalyakin.com>
2024-04-19 11:48:38 +0200

    ignore non-parseable strings

--- src/hooks/parameters.ts
+++ src/hooks/parameters.ts
@@ -6,7 +6,12 @@ export const decodeJsonQueryParameters = (parametersNames: string[]) => async (c
   for (const parameterName of parametersNames) {
     if (query[parameterName] != null) {
       if (typeof query[parameterName] === 'string') {
-        query[parameterName] = JSON.parse(query[parameterName] as string)
+        try {
+          query[parameterName] = JSON.parse(query[parameterName] as string)
+        } catch {
+          // Do nothing - it may be a protobuf filter
+          // if not - this will buble up as an error later
+        }
       } else if (Array.isArray(query[parameterName])) {
         const items = (query[parameterName] as string[]).map(item =>
           typeof item === 'string' ? JSON.parse(item) : item
