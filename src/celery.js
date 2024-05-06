6933f094c72479082eb75f134a048595585eda70
Daniele Guido <gui.daniele@gmail.com>
2024-05-06 16:49:31 +0200

    update celery client

--- src/celery.js
+++ src/celery.js
@@ -1,12 +1,13 @@
-const debug = require('debug')('impresso/celery');
-const celery = require('node-celery');
-const Job = require('./models/jobs.model');
+const debug = require('debug')('impresso/celery')
+const celery = require('celery-node')
+const Job = require('./models/jobs.model')
+const { de } = require('chrono-node')
 
 const JOB_STATUS_TRANSLATIONS = {
   REA: 'A new job has been created',
   RUN: 'Job is doing its job ...',
   DON: 'Job done! Congrats.',
-};
+}
 
 /**
  *
@@ -16,76 +17,53 @@ const JOB_STATUS_TRANSLATIONS = {
  *     CELERY_RESULT_BACKEND: 'redis://localhost/5'
  * }
  * @param  {[type]} config [description]
- * @return {[type]}        [description]
  */
-const getCeleryClient = (config, app) => {
-  const client = celery.createClient(config);
+const getCeleryClient = config => {
+  const client = celery.createClient(config.CELERY_BROKER_URL, config.CELERY_RESULT_BACKEND)
+  debug(
+    'getCeleryClient CELERY_BROKER_URL:',
+    config.CELERY_BROKER_URL,
+    ' CELERY_RESULT_BACKEND:',
+    config.CELERY_RESULT_BACKEND
+  )
+  const run = ({ task = 'impresso.tasks.echo', args = ['this is a test'] } = {}) => {
+    debug(`run celery task ${task}`)
+    const celeryTask = client.createTask(task)
+    const result = celeryTask.applyAsync(args)
+    return result
+      .get()
+      .then(data => {
+        debug('Celery task retrieved!', data)
+        console.log(data)
+        // client.disconnect();
+      })
+      .catch(err => {
+        console.error(err)
+        debug(`Error! ${err}`, err)
+      })
+  }
 
-  client.on('error', err => {
-    debug(`Error! ${err}`);
-  });
-
-  client.on('ready', err => {
-    debug(`ready! ${err}`);
-  });
-  client.on('message', msg => {
-    let result = msg.result;
-
-    if (result && typeof result === 'string') {
-      try {
-        result = JSON.parse(msg);
-      } catch (err) {
-        debug('@message, ERROR, cannot get json from this string:', result);
-        console.error(err);
-      }
-    }
-
-    if (result && typeof result === 'object') {
-      if (result.job) {
-        debug(`@message related to job: ${result.job_id}, send to: ${result.user_uid}`, result);
-        app.service('logs').create({
-          ...result,
-          job: new Job({
-            ...result.job,
-            creationDate: result.job.date_created,
-          }),
-          msg: JOB_STATUS_TRANSLATIONS[result.job.status],
-          to: result.user_uid,
-          from: 'jobs',
-        });
-      } else {
-        debug('@message from unknown origin, cannot propagate:', result);
-      }
-    }
-  });
-
-  client.on('connect', async () => {
-    debug('Celery is ready!');
-  });
-
-  client.run = ({ task = 'echo', args = [] } = {}) =>
-    new Promise((resolve, reject) => {
-      debug(`run celery task ${task}`);
-      client.call(task, args, res => {
-        debug('Celery task retrieved!', res);
-        if (['SUCCESS', 'INIT', 'PROGRESS', 'STOPPED'].indexOf(res.status) !== -1) {
-          resolve(res);
-        } else {
-          reject(res);
-        }
-      });
-    });
-
-  return client;
-};
+  return () => ({
+    run,
+  })
+}
 
 module.exports = function (app) {
-  const config = app.get('celery');
+  const config = app.get('celery')
+  // wait for redis to be ready
   if (!config?.enable) {
-    debug('Celery is not configured. No task management is available.');
-    app.set('celeryClient', null);
+    debug('Celery is not configured. No task management is available.')
+    app.set('celeryClient', null)
   } else {
-    debug("Celery configuration found, let's see if it works...");
-    app.set('celeryClient', getCeleryClient(config, app));
+    debug("Celery configuration found, let's see if it works...")
+    try {
+      const client = getCeleryClient(config)
+      console.log('client', client)
+      client.run()
+      app.set('celeryClient', client)
+    } catch (err) {
+      console.error(err)
+      debug(`Error! ${err}`, err)
+    }
   }
-};
+}
