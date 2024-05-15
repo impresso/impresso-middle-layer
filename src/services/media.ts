612224695bca26d9474bdfad1fae1ad313a5ac33
Daniele Guido <gui.daniele@gmail.com>
2024-05-15 11:13:29 +0200

    create typescript version of media service

--- src/services/media.ts
+++ src/services/media.ts
@@ -1,53 +1,70 @@
-/* eslint global-require: "off" */
-/* eslint import/no-dynamic-require: "off" */
-const debug = require('debug')('impresso/media')
-const { BadRequest, NotFound } = require('@feathersjs/errors')
-// const { authenticate } = require('@feathersjs/authentication');
+import debugModule from 'debug'
+import { Request as ExpressRequest, Response, NextFunction } from 'express'
+import type { Application as ExpressApplication } from '@feathersjs/express'
 
-module.exports = function (app) {
+import { ImpressoApplication } from '../types'
+import User from '../models/users.model'
+const debug = debugModule('impresso/media')
+
+const { BadRequest, NotFound } = require('@feathersjs/errors')
+
+interface Request extends ExpressRequest {
+  user?: User
+}
+
+export default (app: ImpressoApplication & ExpressApplication) => {
   const config = app.get('media')
 
+  if (!config) {
+    debug('Error! Media is not configured. No task management is available.')
+    throw new Error('Error! Media is not configured. No task management is available.')
+  }
   debug('configuring media ...', config.host, config.path)
+  app.use(`${config?.path}/:service/:id`, [
+    function (req: Request, res: Response, next: NextFunction) {
+      if (config.services.indexOf(req.params.service) === -1) {
+        return res.status(400).json({ message: 'Bad param service' })
+      }
 
-  app.use(`${config.path}/:service/:id`, [
+      if (!/^\d+$/.test(req.params.id)) {
+        return res.status(400).json({ message: 'Bad param id' })
+      }
+      next()
+    },
     // authenticate token!
     // authenticate('jwt'),
-    async function (req, res, next) {
+    async function (req: Request, res: Response, next: NextFunction) {
       if (req.method === 'OPTIONS') {
         res.set('Access-Control-Allow-Origin', '*')
         res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         return res.status(200).send()
       }
+      debug('authenticating token ...', req.params.id, req.headers.authorization)
       const token = req.headers.authorization
       if (!token || !token.startsWith('Bearer ')) {
-        throw new BadRequest('Missing or invalid authorization token')
+        debug('missing or invalid authorization token')
+        return res.status(401).json({ message: 'Missing or invalid authorization token' })
       }
       const authToken = token.split(' ')[1]
       const payload = await app.service('/authentication').create({ strategy: 'jwt', accessToken: authToken })
       // Authenticate the token here using your authentication logic
       // For example, you can use a JWT library to verify the token
       // and extract the user information from it
-
-      req.user = payload.user
+      req.user = payload.user as User
       next()
     },
-    function (req, res, next) {
-      if (config.services.indexOf(req.params.service) === -1) {
-        throw new BadRequest('incomplete request params (service)')
+    function (req: Request, res: Response, next: NextFunction) {
+      if (!req.user) {
+        return res.status(401).json({ message: 'Unauthorized' })
       }
-
-      if (!/^\d+$/.test(req.params.id)) {
-        throw new BadRequest('incomplete request params (id)')
-      }
-
       debug(`[${req.params.service}:${req.params.id}] Call ${req.params.service}.get using user uid: ${req.user.uid}`)
       // a class having an attachment
       app
         .service(req.params.service)
         .get(req.params.id, {
           user: req.user,
-        })
+        } as any)
         .then(item => {
           res.locals.item = item
           debug(`[${req.params.service}:${req.params.id}]  ${req.params.service}.get success, check attachments...`)
@@ -57,10 +74,10 @@ module.exports = function (app) {
           next()
         })
         .catch(err => {
-          next(err)
+          return res.status(err.code || 500).json({ message: err.message })
         })
     },
-    function (req, res) {
+    function (req: Request, res: Response) {
       const filename = res.locals.item.attachment.path.split('/').pop()
       debug(`[${req.params.service}:${req.params.id}]`, 'original filepath:', res.locals.item.attachment.path)
       const protectedFilepath = [config.protectedPath, res.locals.item.attachment.path].join('/')
