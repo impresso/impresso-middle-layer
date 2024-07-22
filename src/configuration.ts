552a0614581bbcca07fa3a4847585c2ec939893c
Daniele Guido <gui.daniele@gmail.com>
2024-07-22 12:08:21 +0200

    Fix/version service (#399)
    * Update configuration.ts
    
    * add env variables from process.env directly and use typescript

--- src/configuration.ts
+++ src/configuration.ts
@@ -5,6 +5,7 @@ import type { RedisClientOptions } from 'redis'
 import type { RateLimiterConfiguration } from './services/internal/rateLimiter/redis'
 import { Sequelize } from 'sequelize'
 import { CeleryClient } from './celery'
+import { AuthenticationConfiguration } from '@feathersjs/authentication'
 
 export type RedisConfiguration = RedisClientOptions & { enable?: boolean; host?: string }
 
@@ -21,6 +22,66 @@ export interface MediaConfiguration {
   protectedPath: string
 }
 
+export interface SolrConfiguration {
+  host: string
+  port: number
+  auth: {
+    user: string
+    pass: string
+  }
+  search: {
+    alias: string
+    endpoint: string
+  }
+  mentions: {
+    alias: string
+    endpoint: string
+  }
+  topics: {
+    alias: string
+    endpoint: string
+  }
+  images: {
+    alias: string
+    endpoint: string
+  }
+  entities: {
+    alias: string
+    endpoint: string
+  }
+}
+
+export interface SequelizeConfiguration {
+  alias?: string
+  dialect: string
+  host: string
+  port: number
+  auth: {
+    user: string
+    pass: string
+  }
+  database: string
+  logging?: boolean
+  tables?: {
+    articles: string
+    pages: string
+    newspapers: string
+    users: string
+  }
+}
+
+export interface FeaturesConfiguration {
+  textReuse: {
+    enabled: boolean
+  }
+}
+
+export interface LocalAuthenticationConfiguration extends AuthenticationConfiguration {
+  jwtOptions: {
+    issuer: string
+  }
+}
+
 export interface Configuration {
   isPublicApi?: boolean
   allowedCorsOrigins?: string[]
@@ -29,12 +90,15 @@ export interface Configuration {
   publicApiPrefix?: string
   useDbUserInRequestContext?: boolean
   problemUriBase?: string
-
+  features?: FeaturesConfiguration
   // TODO: move to services:
+  authentication: LocalAuthenticationConfiguration
+  sequelize: SequelizeConfiguration
   sequelizeClient?: Sequelize
   celery?: CeleryConfiguration
   celeryClient?: CeleryClient
   media?: MediaConfiguration
+  solr: SolrConfiguration
 }
 
 const configurationSchema: JSONSchemaDefinition = {
@@ -78,6 +142,54 @@ const configurationSchema: JSONSchemaDefinition = {
       type: 'string',
       description:
         'Base URI for problem URIs. Falls back to the default URI (https://impresso-project.ch/probs) if not set',
+    },
+    solr: {
+      type: 'object',
+      properties: {
+        host: { type: 'string', description: 'Solr host' },
+        port: { type: 'number', description: 'Solr port' },
+        auth: {
+          type: 'object',
+          properties: {
+            user: { type: 'string', description: 'Solr user' },
+            pass: { type: 'string', description: 'Solr password' },
+          },
+          required: ['user', 'pass'],
+        },
+      },
+      description: 'Solr configuration',
+      required: ['host', 'port', 'auth'],
+    },
+    sequelize: {
+      type: 'object',
+      properties: {
+        alias: { type: 'string', description: 'Alias for the Sequelize instance' },
+        dialect: { type: 'string', description: 'Dialect of the database' },
+        host: { type: 'string', description: 'Host of the database' },
+        port: { type: 'number', description: 'Port of the database' },
+        auth: {
+          type: 'object',
+          properties: {
+            user: { type: 'string', description: 'Database user' },
+            pass: { type: 'string', description: 'Database password' },
+          },
+          required: ['user', 'pass'],
+        },
+        database: { type: 'string', description: 'Database name' },
+        logging: { type: 'boolean', description: 'Enable logging' },
+        tables: {
+          type: 'object',
+          properties: {
+            articles: { type: 'string', description: 'Name of the articles table' },
+            pages: { type: 'string', description: 'Name of the pages table' },
+            newspapers: { type: 'string', description: 'Name of the newspapers table' },
+            users: { type: 'string', description: 'Name of the users table' },
+          },
+          required: ['articles', 'pages', 'newspapers', 'users'],
+        },
+      },
+      description: 'Sequelize configuration',
+      required: ['dialect', 'host', 'port', 'auth', 'database'],
     },
   },
 } as const
