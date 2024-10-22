4f61fd73123d38a8798ba0670f80c291d7efd463
Roman Kalyakin <roman@kalyakin.com>
2024-10-22 13:59:03 +0200

    Renamed services to content items (#447)

---
+++ src/services/articles/articles.service.ts
@@ -0,0 +1,43 @@
+import { ServiceOptions } from '@feathersjs/feathers'
+import { createSwaggerServiceOptions } from 'feathers-swagger'
+import { ImpressoApplication } from '../../types'
+import { docs } from './articles.schema'
+
+// Initializes the `articles` service on path `/articles`
+const createService = require('./articles.class')
+const hooks = require('./articles.hooks')
+
+export default function (app: ImpressoApplication) {
+  const paginate = app.get('paginate')
+
+  /**
+   * Even though the service is historically called 'articles'
+   * it technically deals with content items. We keep the original prefix
+   * for the internal use (web app), but expose it as 'content-items' for the public API.
+   */
+  const prefix = app.get('isPublicApi') ? '/content-items' : '/articles'
+
+  const options = {
+    name: 'articles',
+    paginate,
+    app,
+  }
+
+  const svc = createService(options)
+
+  // Initialize our service with any options it requires
+  app.use('/content-items', svc, {
+    events: [],
+    docs: createSwaggerServiceOptions({ schemas: {}, docs }),
+  } as ServiceOptions)
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('content-items')
+
+  service.hooks(hooks)
+
+  if (!app.get('isPublicApi')) {
+    // Expose the service as 'articles' for the web app
+    app.use('/articles', service)
+  }
+}
