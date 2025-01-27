a8106701ce3e50b4f036eb8ea55dcd24bbd77263
Roman Kalyakin <roman@kalyakin.com>
2025-01-27 16:28:39 +0100

    fixed articles suggestions (#497)

--- src/jobs/updateTopicsCache.ts
+++ src/jobs/updateTopicsCache.ts
@@ -1,7 +1,9 @@
 import { prepareTopics } from '../useCases/prepareTopics'
 import { WellKnownKeys } from '../cache'
 import { ImpressoApplication } from '../types'
-import { SolrNamespaces } from '../solr'
+
+/** 100 days */
+const DefaultTtlSeconds = 60 * 60 * 24 * 100
 
 /**
  * Prepare topics and store them in cache.
@@ -10,8 +12,11 @@ const run = async (app: ImpressoApplication) => {
   const cache = app.get('cacheManager')
   const solrClient = app.service('simpleSolrClient')
 
+  const cached = await cache.get(WellKnownKeys.Topics)
+  if (cached != null) return
+
   const topics = await prepareTopics(solrClient)
-  await cache.set(WellKnownKeys.Topics, JSON.stringify(topics))
+  await cache.set(WellKnownKeys.Topics, JSON.stringify(topics), DefaultTtlSeconds)
 }
 
 export default run
--- src/services/articles-suggestions/articles-suggestions.service.js
+++ src/services/articles-suggestions/articles-suggestions.service.js
@@ -1,18 +1,13 @@
 // Initializes the `articles-suggestions` service on path `/articles-suggestions`
-const createService = require('./articles-suggestions.class.js');
-const hooks = require('./articles-suggestions.hooks');
+import { ArticlesSuggestionsService } from './articles-suggestions.class'
+const hooks = require('./articles-suggestions.hooks')
 
 module.exports = function (app) {
-  const options = {
-    app,
-    name: 'articles-suggestions',
-  };
-
   // Initialize our service with any options it requires
-  app.use('/articles-suggestions', createService(options));
+  app.use('/articles-suggestions', new ArticlesSuggestionsService({ solr: app.service('simpleSolrClient') }))
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('articles-suggestions');
+  const service = app.service('articles-suggestions')
 
-  service.hooks(hooks);
-};
+  service.hooks(hooks)
+}
