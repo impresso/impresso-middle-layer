0efa497a36ddf4e20bbaec3b2c10c2a493ba06eb
Roman Kalyakin <roman@kalyakin.com>
2025-02-05 19:37:13 +0300

    Fixed escaping of entities ID values

--- src/util/solr/filterBuilders/value.ts
+++ src/util/solr/filterBuilders/value.ts
@@ -4,7 +4,7 @@ import { FilterBuilderFn, FilterField } from './types'
 
 export const escapeString = (s: string): string => s.replace(/[()\\+&|!{}[\]?:;,]/g, d => `\\${d}`)
 
-const valueBuilder: FilterBuilderFn = (filters: Filter[], filterField: FilterField, ruleName: string) => {
+export const valueBuilder: FilterBuilderFn = (filters: Filter[], filterField: FilterField, ruleName: string) => {
   return baseFilterBuilderFn(
     filters,
     filterField,
@@ -15,4 +15,27 @@ const valueBuilder: FilterBuilderFn = (filters: Filter[], filterField: FilterFie
   )
 }
 
-export default valueBuilder
+export const escapeIdValue = (value: string) => {
+  return escape(value).replace(/%([0-9a-f]{2})/gi, (_, hex) => `$${hex.toLowerCase()}$`)
+}
+
+export const unescapeIdValue = (value: string) => {
+  return unescape(value.replace(/\$([0-9a-f]{2})\$/gi, '%$1'))
+}
+
+/**
+ * Similar to `value` filter builder but uses a different escape function
+ * designed to work with IDs. E.g.:
+ * `aida-0001-50-Poseidon_(film)` -> `pers_entities_dpfs:aida-0001-50-Poseidon_$28$film$29$`
+ */
+export const idValueBuilder: FilterBuilderFn = (filters: Filter[], filterField: FilterField, ruleName: string) => {
+  return baseFilterBuilderFn(
+    filters,
+    filterField,
+    ruleName,
+    // unescape before escaping, in case it has already been escaped
+    x => escapeIdValue(unescapeIdValue(x)),
+    defaultItemBuilder,
+    noPrefixFilterFieldConverter
+  )
+}
