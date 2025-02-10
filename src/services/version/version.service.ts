8f0e4f131f523f71b58a08c58cc724add2328a70
Daniele Guido <gui.daniele@gmail.com>
2025-02-10 10:01:00 +0100

    Update version.service.ts

--- src/services/version/version.service.ts
+++ src/services/version/version.service.ts
@@ -22,9 +22,9 @@ module.exports = function (app: ImpressoApplication) {
         const cache = app.get('cacheManager')
         const [firstDate, lastDate] = await getFirstAndLastDocumentDates(solr)
         log('branch:', process.env.GIT_BRANCH, 'revision:', process.env.GIT_REVISION, 'version:', process.env.GIT_TAG)
-        const ms = await cache.get<string>(WellKnownKeys.MediaSources)
-        const deserialisedResult: { uid: string; name: string; type: string }[] = JSON.parse(ms ?? '[]')
-
+        const mediaSources = app.service('media-sources')
+        const lookup = await mediaSources.getLookup()
+        log(lookup)
         const response: VersionDetails = {
           solr: {
             endpoints: {},
@@ -39,16 +39,17 @@ module.exports = function (app: ImpressoApplication) {
             version: process.env.GIT_TAG || 'N/A',
           },
           documentsDateSpan: { firstDate, lastDate },
-          newspapers: deserialisedResult.reduce(
-            (acc: Record<string, Record<string, any>>, d: { uid: string; name: string; type: string }) => {
-              acc[d.uid] = {
-                name: d.name,
-                type: d.type,
-              }
-              return acc
-            },
-            {}
-          ),
+          newspapers: lookup as Record<string, Record<string, any>>,
+          // newspapers: lookup.reduce(
+          //   (acc: Record<string, Record<string, any>>, d: { uid: string; name: string; type: string }) => {
+          //     acc[d.uid] = {
+          //       name: d.name,
+          //       type: d.type,
+          //     }
+          //     return acc
+          //   },
+          //   {}
+          // ),
           features: (app.get('features') ?? {}) as Record<string, Record<string, any>>,
         }
 
