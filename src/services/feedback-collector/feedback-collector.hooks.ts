c669cff66a286c9589d0ef09b7a0df45ae8a606a
Daniele Guido <gui.daniele@gmail.com>
2025-02-28 12:30:36 +0100

    use JSON schema

--- src/services/feedback-collector/feedback-collector.hooks.ts
+++ src/services/feedback-collector/feedback-collector.hooks.ts
@@ -1,39 +1,11 @@
 import { authenticateAround as authenticate } from '../../hooks/authenticate'
-import { validate } from '../../hooks/params'
-
-const FeedbackOptionContentItemMetadataIssue = 'ContentItemMetadataIssue'
-const FeedbackOptionLayoutSegmentationIssue = 'LayoutSegmentationIssue'
-const FeedbackOptionDocumentLoadingIssue = 'DocumentLoadingIssue'
-const FeedbackOptionOtherIssue = 'OtherIssue'
+import { validateWithSchema } from '../../hooks/schema'
 
 export default {
   around: {
     all: [authenticate({ allowUnauthenticated: true })],
   },
   before: {
-    create: [
-      validate(
-        {
-          // request must contain a name - from which we will create a UID
-          issue: {
-            required: false,
-            choices: [
-              FeedbackOptionContentItemMetadataIssue,
-              FeedbackOptionLayoutSegmentationIssue,
-              FeedbackOptionDocumentLoadingIssue,
-              FeedbackOptionOtherIssue,
-            ],
-            defaultValue: FeedbackOptionOtherIssue,
-          },
-          content: {
-            max_length: 500,
-          },
-          route: {
-            max_length: 500,
-          },
-        },
-        'POST'
-      ),
-    ],
+    create: [validateWithSchema('services/feedback-collector/schema/create/payload.json')],
   },
 }
