e0eb56028b7fd7a9539bc7950d370a8a4d85d8a6
Roman Kalyakin <roman@kalyakin.com>
2025-04-16 10:58:48 +0200

    load facet ranges as a part of a job (#546)
    * moved script to jobs
    
    * always update facet ranges

---
+++ src/jobs/updateFacetRanges.ts
@@ -0,0 +1,47 @@
+import { ImpressoApplication } from '../types'
+import { loadFacetRanges } from '../useCases/loadFacetRanges'
+import { writeFile, access, constants } from 'node:fs/promises'
+
+const CacheFileName = './data/facetRanges.json'
+
+const cacheExists = async () => {
+  try {
+    await access(CacheFileName, constants.R_OK)
+    return true
+  } catch {
+    return false
+  }
+}
+
+const writeCache = async (data: any) => {
+  await writeFile(CacheFileName, JSON.stringify(data), { encoding: 'utf8' })
+}
+/**
+ * Prepare facet ranges and store them in cache.
+ *
+ * NOTE: Using file as a cache because static code in
+ * IML depends on it. Cache can be moved to Redis later
+ * when the static code is refactored.
+ */
+const run = async (app: ImpressoApplication, reload: boolean = true) => {
+  const solrClient = app.service('simpleSolrClient')
+
+  // Check cache first
+  const exists = await cacheExists()
+  if (exists && !reload) {
+    console.log('Facet ranges data found in cache, skipping update.')
+    return
+  }
+
+  console.log('Updating facet ranges data...')
+  try {
+    const ranges = await loadFacetRanges(solrClient)
+    await writeCache(ranges)
+    console.log(`Successfully updated and cached facet ranges data.`)
+  } catch (error) {
+    console.error('Error updating facet ranges data:', error)
+    // Decide if the error should be re-thrown or handled
+  }
+}
+
+export default run
