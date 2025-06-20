b536fe0dc7d5a49be23b745b61a369fed1d8481a
Roman Kalyakin <roman@kalyakin.com>
2025-06-20 13:34:43 +0200

    updated methods exposure in content-items

--- src/services/content-items/content-items.service.ts
+++ src/services/content-items/content-items.service.ts
@@ -6,21 +6,14 @@ import createService from './content-items.class'
 import hooks from './content-items.hooks'
 
 export default function (app: ImpressoApplication) {
-  const paginate = app.get('paginate')
-
-  /**
-   * Even though the service is historically called 'articles'
-   * it technically deals with content items. We keep the original prefix
-   * for the internal use (web app), but expose it as 'content-items' for the public API.
-   */
-  const prefix = app.get('isPublicApi') ? '/content-items' : '/articles'
+  const isPublicApi = app.get('isPublicApi') ?? false
 
   const svc = createService({ app })
 
   // Initialize our service with any options it requires
   app.use('/content-items', svc, {
     events: [],
-    methods: ['get'],
+    methods: isPublicApi ? ['get'] : ['get', 'find'],
     docs: createSwaggerServiceOptions({ schemas: {}, docs }),
   } as ServiceOptions)
 
