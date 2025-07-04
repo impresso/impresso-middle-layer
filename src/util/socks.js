df2416d5f91f48cd188c96e3681cdac6cba14838
Roman Kalyakin <roman@kalyakin.com>
2025-07-04 17:19:36 +0200

    fixed linter issues

--- src/util/socks.js
+++ src/util/socks.js
@@ -1,20 +1,24 @@
-var stream = require('stream'),
-  util = require('util'),
-  net = require('net'),
-  tls = require('tls'),
-  ipaddr = require('ipaddr.js')
+/**
+ * From deprecated `socks` module, which is no longer maintained.
+ */
 
-var SocksConnection = function (remote_options, socks_options) {
-  var that = this
+const stream = require('stream')
+const util = require('util')
+const net = require('net')
+const tls = require('tls')
+const ipaddr = require('ipaddr.js')
+
+const SocksConnection = function (remoteOptions, socksOptions) {
+  const that = this
 
   stream.Duplex.call(this)
 
-  this.remote_options = defaults(remote_options, {
+  this.remoteOptions = defaults(remoteOptions, {
     host: 'localhost',
     ssl: false,
     rejectUnauthorized: false,
   })
-  socks_options = defaults(socks_options, {
+  socksOptions = defaults(socksOptions, {
     localAddress: '0.0.0.0',
     allowHalfOpen: false,
     host: 'localhost',
@@ -30,35 +34,35 @@ var SocksConnection = function (remote_options, socks_options) {
 
   this.socksSocket = net.createConnection(
     {
-      host: socks_options.host,
-      port: socks_options.port,
-      localAddress: socks_options.localAddress,
-      allowHalfOpen: socks_options.allowHalfOpen,
+      host: socksOptions.host,
+      port: socksOptions.port,
+      localAddress: socksOptions.localAddress,
+      allowHalfOpen: socksOptions.allowHalfOpen,
     },
-    socksConnected.bind(this, !!socks_options.user)
+    socksConnected.bind(this, !!socksOptions.user)
   )
 
   this.socksSocket.on('error', function (err) {
     that.emit('error', err)
   })
 
-  socksAuth.call(this, { user: socks_options.user, pass: socks_options.pass })
+  socksAuth.call(this, { user: socksOptions.user, pass: socksOptions.pass })
 
   this.outSocket = this.socksSocket
 }
 
 util.inherits(SocksConnection, stream.Duplex)
 
-SocksConnection.connect = function (remote_options, socks_options, connection_listener) {
-  var socks_connection = new SocksConnection(remote_options, socks_options)
-  if (typeof connection_listener === 'function') {
-    socks_connection.on('connect', connection_listener)
+SocksConnection.connect = function (remoteOptions, socksOptions, connectionListener) {
+  const socksConnection = new SocksConnection(remoteOptions, socksOptions)
+  if (typeof connectionListener === 'function') {
+    socksConnection.on('connect', connectionListener)
   }
-  return socks_connection
+  return socksConnection
 }
 
 SocksConnection.prototype._read = function () {
-  var data
+  let data
   if (this._socksSetup) {
     while ((data = this.outSocket.read()) !== null) {
       if (this.push(data) === false) {
@@ -74,6 +78,7 @@ SocksConnection.prototype._write = function (chunk, encoding, callback) {
   if (this._socksSetup) {
     this.outSocket.write(chunk, 'utf8', callback)
   } else {
+    // eslint-disable-next-line n/no-callback-literal
     callback('Not connected')
   }
 }
@@ -88,9 +93,9 @@ SocksConnection.prototype.dispose = function () {
   this.removeAllListeners()
 }
 
-var getData = function (socket, bytes, callback) {
-  var dataReady = function () {
-    var data = socket.read(bytes)
+const getData = function (socket, bytes, callback) {
+  const dataReady = function () {
+    const data = socket.read(bytes)
     if (data !== null) {
       socket.removeListener('readable', dataReady)
       callback(data)
@@ -101,7 +106,7 @@ var getData = function (socket, bytes, callback) {
   dataReady()
 }
 
-var socksConnected = function (auth) {
+const socksConnected = function (auth) {
   if (auth) {
     this.socksSocket.write('\x05\x02\x02\x00') // SOCKS version 5, supporting two auth methods
     // username/password and 'no authentication'
@@ -110,8 +115,8 @@ var socksConnected = function (auth) {
   }
 }
 
-var socksAuth = function (auth) {
-  var that = this
+const socksAuth = function (auth) {
+  const that = this
   getData(this.socksSocket, 2, function (data) {
     if (data.readUInt8(0) !== 5) {
       that.emit('error', 'Only SOCKS version 5 is supported')
@@ -126,26 +131,26 @@ var socksAuth = function (auth) {
       case 2:
         that.socksSocket.write(
           Buffer.concat([
-            new Buffer([1]),
-            new Buffer([Buffer.byteLength(auth.user)]),
-            new Buffer(auth.user),
-            new Buffer([Buffer.byteLength(auth.pass)]),
-            new Buffer(auth.pass),
+            Buffer.from([1]),
+            Buffer.from([Buffer.byteLength(auth.user)]),
+            Buffer.from(auth.user),
+            Buffer.from([Buffer.byteLength(auth.pass)]),
+            Buffer.from(auth.pass),
           ])
         )
         socksAuthStatus.call(that)
         break
       default:
-        socksRequest.call(that, that.remote_options.host, that.remote_options.port)
+        socksRequest.call(that, that.remoteOptions.host, that.remoteOptions.port)
     }
   })
 }
 
-var socksAuthStatus = function (data) {
-  var that = this
+const socksAuthStatus = function (data) {
+  const that = this
   getData(this.socksSocket, 2, function (data) {
     if (data.readUInt8(1) === 0) {
-      socksRequest.call(that, that.remote_options.host, that.remote_options.port)
+      socksRequest.call(that, that.remoteOptions.host, that.remoteOptions.port)
     } else {
       that.emit('error', 'SOCKS: Authentication failed')
       that.socksSocket.destroy()
@@ -153,84 +158,81 @@ var socksAuthStatus = function (data) {
   })
 }
 
-var socksRequest = function (host, port) {
-  var header, type, hostBuf, portBuf
+const socksRequest = function (host, port) {
+  let type, hostBuf
   if (net.isIP(host)) {
     if (net.isIPv4(host)) {
-      type = new Buffer([1])
+      type = Buffer.from([1])
     } else if (net.isIPv6(host)) {
-      type = new Buffer([4])
+      type = Buffer.from([4])
     }
-    hostBuf = new Buffer(ipaddr.parse(host).toByteArray())
+    hostBuf = Buffer.from(ipaddr.parse(host).toByteArray())
   } else {
-    type = new Buffer([3])
-    hostBuf = new Buffer(host)
-    hostBuf = Buffer.concat([new Buffer([Buffer.byteLength(host)]), hostBuf])
+    type = Buffer.from([3])
+    hostBuf = Buffer.from(host)
+    hostBuf = Buffer.concat([Buffer.from([Buffer.byteLength(host)]), hostBuf])
   }
-  header = new Buffer([5, 1, 0])
-  portBuf = new Buffer(2)
+  const header = Buffer.from([5, 1, 0])
+  const portBuf = Buffer.alloc(2)
   portBuf.writeUInt16BE(port, 0)
   this.socksSocket.write(Buffer.concat([header, type, hostBuf, portBuf]))
   socksReply.call(this)
 }
 
-var socksReply = function (data) {
-  var that = this
+const socksReply = function (data) {
+  const that = this
   getData(this.socksSocket, 4, function (data) {
-    var status, err, cont
+    let err
 
-    cont = function (addr, port) {
+    const cont = function (addr, port) {
       that.socksAddress = addr
       that.socksPort = port
 
-      if (that.remote_options.ssl) {
+      if (that.remoteOptions.ssl) {
         startTLS.call(that)
       } else {
         proxyData.call(that)
         that.emit('connect')
       }
     }
-    status = data.readUInt8(1)
+    const status = data.readUInt8(1)
     if (status === 0) {
       switch (data.readUInt8(3)) {
         case 1:
           getData(that.socksSocket, 6, function (data2) {
-            var addr = '',
-              port,
-              i
+            let addr = ''
+            let i
             for (i = 0; i < 4; i++) {
               if (i !== 0) {
                 addr += '.'
               }
               addr += data2.readUInt8(i).toString()
             }
-            port = data2.readUInt16BE(4)
+            const port = data2.readUInt16BE(4)
             cont(addr, port)
           })
           break
         case 3:
           getData(that.socksSocket, 1, function (data2) {
-            var length = data2.readUInt8(0)
+            const length = data2.readUInt8(0)
             getData(that.socksSocket, length + 2, function (data3) {
-              var addr, port
-              addr = data3.slice(0, -2).toString()
-              port = data3.readUInt16BE(length)
+              const addr = data3.slice(0, -2).toString()
+              const port = data3.readUInt16BE(length)
               cont(addr, port)
             })
           })
           break
         case 4:
           getData(that.socksSocket, 18, function (data2) {
-            var addr = '',
-              port,
-              i
+            let addr = ''
+            let i
             for (i = 0; i < 16; i++) {
               if (i !== 0) {
                 addr += ':'
               }
               addr += data2.readUInt8(i)
             }
-            port = data2.readUInt16BE(16)
+            const port = data2.readUInt16BE(16)
             cont(addr, port)
           })
           break
@@ -273,14 +275,14 @@ var socksReply = function (data) {
   })
 }
 
-var startTLS = function () {
-  var that = this
-  var plaintext = tls.connect({
+const startTLS = function () {
+  const that = this
+  const plaintext = tls.connect({
     socket: this.socksSocket,
-    rejectUnauthorized: this.remote_options.rejectUnauthorized,
-    key: this.remote_options.key,
-    cert: this.remote_options.cert,
-    requestCert: this.remote_options.requestCert,
+    rejectUnauthorized: this.remoteOptions.rejectUnauthorized,
+    key: this.remoteOptions.key,
+    cert: this.remoteOptions.cert,
+    requestCert: this.remoteOptions.requestCert,
   })
 
   plaintext.on('error', function (err) {
@@ -299,11 +301,11 @@ var startTLS = function () {
   proxyData.call(this)
 }
 
-var proxyData = function () {
-  var that = this
+const proxyData = function () {
+  const that = this
 
   this.outSocket.on('readable', function () {
-    var data
+    let data
     while ((data = that.outSocket.read()) !== null) {
       if (that.push(data) === false) {
         break
@@ -315,17 +317,17 @@ var proxyData = function () {
     that.push(null)
   })
 
-  this.outSocket.on('close', function (had_err) {
-    that.emit('close', had_err)
+  this.outSocket.on('close', function (hadErr) {
+    that.emit('close', hadErr)
   })
 
   this._socksSetup = true
 }
 
-var defaults = function (obj) {
+const defaults = function (obj) {
   Array.prototype.slice.call(arguments, 1).forEach(function (source) {
     if (source) {
-      for (var prop in source) {
+      for (const prop in source) {
         if (obj[prop] === null) {
           obj[prop] = source[prop]
         }
