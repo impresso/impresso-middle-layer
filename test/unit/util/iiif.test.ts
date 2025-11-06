5be9e0edcd51b684814283d770b31373c9048905
Roman Kalyakin <roman@kalyakin.com>
2025-11-06 16:40:57 +0100

    moved tests to a dedicated directory

---
+++ test/unit/util/iiif.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert'
+import { getV3CompatibleIIIFUrl } from '../../../src/util/iiif'
+
+describe('getV3CompatibleIIIFUrl', () => {
+  it('should convert a v2 URL with full to v3 with max', () => {
+    const v2Url = 'http://example.com/iiif/image1/full/full/0/default.jpg'
+    const expectedV3Url = 'http://example.com/iiif/image1/full/max/0/default.jpg'
+    assert.strictEqual(getV3CompatibleIIIFUrl(v2Url), expectedV3Url)
+  })
+
+  it('should return the same URL if already v3 compatible', () => {
+    const v3Url = 'http://example.com/iiif/image2/10,20,30,40/pct:50/90/color.png'
+    assert.strictEqual(getV3CompatibleIIIFUrl(v3Url), v3Url)
+  })
+})
---
+++ test/unit/util/redaction.test.ts
@@ -0,0 +1,62 @@
+import { RedactionPolicy, redactObject } from '../../../src/util/redaction'
+import assert from 'assert'
+
+interface TestDocument {
+  title: string
+  images: { url: string }[]
+  secret?: string
+}
+
+const incorrectInputs = [null, [1, 2, 3], 1, 'string', false, undefined]
+
+describe('redactObject', () => {
+  it('redacts object successfully', () => {
+    const policy = {
+      name: 'test',
+      items: [
+        {
+          jsonPath: '$.title',
+          valueConverterName: 'redact',
+        },
+        {
+          jsonPath: '$.images[*].url',
+          valueConverterName: 'contextNotAllowedImage',
+        },
+        {
+          jsonPath: '$.secret',
+          valueConverterName: 'remove',
+        },
+      ],
+    } satisfies RedactionPolicy
+
+    const input = {
+      title: 'This is a title',
+      images: [{ url: 'https://example.com/image1.jpg' }],
+      secret: 'This is a secret',
+    } satisfies TestDocument
+
+    const expectedOutput = {
+      title: '[REDACTED]',
+      images: [{ url: 'https://impresso-project.ch/assets/images/not-allowed.png' }],
+      secret: undefined,
+    } satisfies TestDocument
+
+    assert.deepEqual(redactObject(input, policy), expectedOutput)
+  })
+
+  incorrectInputs.forEach(input => {
+    it(`fails to redact unknown type of input: ${input}`, () => {
+      const policy = {
+        name: 'test',
+        items: [
+          {
+            jsonPath: '$.title',
+            valueConverterName: 'redact',
+          },
+        ],
+      } satisfies RedactionPolicy
+
+      assert.throws(() => redactObject(input as any, policy), Error, 'The provided object is not Redactable')
+    })
+  })
+})
