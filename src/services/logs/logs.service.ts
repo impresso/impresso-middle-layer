b4856e2c9a9a78582a00f75b206b2f29b67dccff
Roman Kalyakin <roman@kalyakin.com>
2025-12-08 14:59:41 +0100

    do not attempt to get schema for an internal "logs" service

--- src/services/logs/logs.service.ts
+++ src/services/logs/logs.service.ts
@@ -1,9 +1,11 @@
-import { ImpressoApplication } from '../../types'
 import Debug from 'debug'
-const debug = Debug('impresso/services:logs')
+import { optionsDisabledInPublicApi } from '../../hooks/public-api'
+import { ImpressoApplication } from '../../types'
 import createService from './logs.class'
+
+const debug = Debug('impresso/services:logs')
 
 export default function (app: ImpressoApplication) {
   debug('Registering logs service')
-  app.use('/logs', createService())
+  app.use('/logs', createService(), optionsDisabledInPublicApi(app))
 }
