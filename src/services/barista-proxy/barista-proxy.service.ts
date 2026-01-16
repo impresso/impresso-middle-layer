a66f0c40f70f35272768b12a856e4c5f2a9dff9f
Roman Kalyakin <roman@kalyakin.com>
2026-01-16 16:43:57 +0100

    workign version of streaming barista proxy

--- src/services/barista-proxy/barista-proxy.service.ts
+++ src/services/barista-proxy/barista-proxy.service.ts
@@ -3,6 +3,14 @@ import { BaristaProxy } from '@/services/barista-proxy/barista-proxy.class.js'
 import hooks from '@/services/barista-proxy/barista-proxy.hooks.js'
 
 export default function (app: ImpressoApplication) {
-  app.use('/barista-proxy', new BaristaProxy(app.get('features')?.barista!))
+  app.use('/barista-proxy', new BaristaProxy(app, app.get('features')?.barista!), {
+    events: ['barista-response'],
+  })
   app.service('barista-proxy').hooks(hooks)
+
+  // Publish barista-response events to authenticated users
+  app.service('barista-proxy').publish('barista-response', (data: any, context: any) => {
+    // Send to authenticated channel for logged-in users
+    return app.channel('authenticated')
+  })
 }
